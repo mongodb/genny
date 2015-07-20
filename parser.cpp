2732292dc3a69201bf4c021d3ce27f8e03545586
dalyd <david.daly@mongodb.com>
2015-07-20 12:03:19 -0400

    Added in main node and follow it's indirection

--- parser.cpp
+++ parser.cpp
@@ -9,16 +9,23 @@
 
 using namespace std;
 
-int main()
-{
-    YAML::Node nodes = YAML::LoadFile("sample.yml");
+int main() {
+YAML::Node nodes = YAML::LoadFile("sample.yml");
 
-    if (nodes["random query"] ) 
-        cout << "I have random query" << endl;
+if (nodes["random query"] ) 
+    cout << "I have random query" << endl;
 
-    for (auto node : nodes)
-        cout << node.first << endl;
+for (auto node : nodes)
+    cout << node.first << endl;
 
-    //    cout << nodes << endl;
+// Look for main. And start building from there. 
+ if ( auto main = nodes["main"])
+     {
+         cout << "Have main here: " << main << endl;
+         auto workload = nodes[main.Scalar()];
+         cout << "Have real workload here: " << workload << endl;
+     }
 
+ else
+     cout << "There was no main" << endl;
 }
