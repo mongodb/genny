a8560c04221b8d10a7311c60023874076b91498d
dalyd <david.daly@mongodb.com>
2015-07-20 18:20:33 -0400

    A lot of progress

--- Makefile
+++ Makefile
@@ -2,8 +2,11 @@ SHELL = /bin/sh
 .SUFFIXES:
 .SUFFIXES: .c .cpp .o
 
-CC=g++
-CCFLAGS=-l yaml-cpp -std=c++11 -I/usr/local/include/mongocxx/v0.3 -I/usr/local/include/libmongoc-1.0 -I/usr/local/include/bsoncxx/v0.3 -I/usr/local/include/libbson-1.0 -L/usr/local/lib -lmongocxx -lbsoncxx 
+CPP=g++
+CPPFLAGS=-l yaml-cpp -std=c++11 -I/usr/local/include/mongocxx/v0.3 -I/usr/local/include/libmongoc-1.0 -I/usr/local/include/bsoncxx/v0.3 -I/usr/local/include/libbson-1.0 -L/usr/local/lib -lmongocxx -lbsoncxx -lbson
+
+query.o : query.cpp
 
 mwg : parser.cpp
-	$(CC) $< -I.  $(CCFLAGS) -o $@
+	$(CPP) $< -I.  $(CPPFLAGS) -o $@
+
--- parser.cpp
+++ parser.cpp
@@ -3,6 +3,9 @@
 #include "yaml-cpp/yaml.h"
 #include <bsoncxx/builder/stream/document.hpp>
 #include <bsoncxx/json.hpp>
+#include <bson.h>
+#include <vector>
+
 
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
@@ -10,22 +13,27 @@
 using namespace std;
 
 int main() {
-YAML::Node nodes = YAML::LoadFile("sample.yml");
+  YAML::Node nodes = YAML::LoadFile("sample.yml");
 
-if (nodes["random query"] ) 
+  if (nodes["random query"])
     cout << "I have random query" << endl;
 
-for (auto node : nodes)
+  for (auto node : nodes)
     cout << node.first << endl;
 
-// Look for main. And start building from there. 
- if ( auto main = nodes["main"])
-     {
-         cout << "Have main here: " << main << endl;
-         auto workload = nodes[main.Scalar()];
-         cout << "Have real workload here: " << workload << endl;
-     }
+  // Look for main. And start building from there.
+  if (auto main = nodes["main"]) {
+    cout << "Have main here: " << main << endl;
+    auto workload = nodes[main.Scalar()];
+    if (workload)
+      cout << "Have real workload here: " << workload << endl;
+    else
+      cout << "Error -- workload defined by main doesn't exist" << endl;
+cout << nodes["query"]["query"] << endl;
+  }
 
- else
-     cout << "There was no main" << endl;
+  else
+    cout << "There was no main" << endl;
+
+query myquery{workload};
 }
