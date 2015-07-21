922b794b2fae45673a94876a16c0243a5f7e7c59
dalyd <david.daly@mongodb.com>
2015-07-21 10:24:26 -0400

    Have some valid bson. It's a start

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ SHELL = /bin/sh
 .SUFFIXES: .c .cpp .o
 
 CPP=g++
-CPPFLAGS=-l yaml-cpp -std=c++11 -I/usr/local/include/mongocxx/v0.3 -I/usr/local/include/libmongoc-1.0 -I/usr/local/include/bsoncxx/v0.3 -I/usr/local/include/libbson-1.0 -L/usr/local/lib -lmongocxx -lbsoncxx -lbson
+CPPFLAGS=-l yaml-cpp -std=c++11 -I/usr/local/include/mongocxx/v0.3 -I/usr/local/include/libmongoc-1.0 -I/usr/local/include/bsoncxx/v0.3 -I/usr/local/include/libbson-1.0 -L/usr/local/lib -lmongocxx -lbsoncxx  -lbson-1.0 
 
-query.o : query.cpp
+query.o : query.cpp query.hpp
 
-mwg : parser.cpp
-	$(CPP) $< -I.  $(CPPFLAGS) -o $@
+mwg : parser.cpp query.o
+	$(CPP) $< -I.  $(CPPFLAGS) -o $@ query.o
 
--- parser.cpp
+++ parser.cpp
@@ -10,7 +10,10 @@
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 
+#include "query.hpp"
+
 using namespace std;
+using namespace mwg;
 
 int main() {
   YAML::Node nodes = YAML::LoadFile("sample.yml");
@@ -29,11 +32,30 @@ int main() {
       cout << "Have real workload here: " << workload << endl;
     else
       cout << "Error -- workload defined by main doesn't exist" << endl;
-cout << nodes["query"]["query"] << endl;
+    cout << nodes["query"]["query"] << endl;
+    mongocxx::instance inst{};
+    mongocxx::client conn{};
+string jsonstring{"{\"x\":1}\n"};
+cout << jsonstring.c_str() << endl;
+
+
+if (bson_utf8_validate(jsonstring.c_str(), jsonstring.length() ,false))
+    cout << "Validate utf8" << endl;
+ else 
+     cout << "invalid utf8" << endl;
+bson_error_t bsonerror;
+auto mquery = bson_new_from_json((const unsigned char *) jsonstring.c_str(), jsonstring.length(), &bsonerror);
+//bson_t* result = bson_new_from_json(
+//reinterpret_cast<const uint8_t*>(jsonstring.data()), jsonstring.size(), &bsonerror);
+if (mquery)
+    cout << "Yeah -- we have some bson" << endl;
+ else
+     cout << bsonerror.message << endl;
+
+query myquery(workload);
+myquery.execute(conn);
   }
 
   else
     cout << "There was no main" << endl;
-
-query myquery{workload};
 }
