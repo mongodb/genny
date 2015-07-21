6b82f5eeb8b0582b06add5401749978a5c7c70ca
dalyd <david.daly@mongodb.com>
2015-07-21 15:00:09 -0400

    Added insert operation

---
+++ insert.cpp
@@ -0,0 +1,33 @@
+#include "insert.hpp"
+#include<stdlib.h>
+
+namespace mwg {
+
+    insert::insert(YAML::Node &node) {
+        // need to set the name
+        // these should be made into exceptions
+        // should be a map, with type = insert
+        if (!node) {
+            cerr << "Insert constructor and !node" << endl;
+            exit(EXIT_FAILURE);
+            }
+        if (!node.IsMap()) {
+            cerr << "Not map in insert type initializer" << endl;
+            exit(EXIT_FAILURE);
+        }
+        if (node["type"].Scalar() != "insert") {
+                cerr << "Insert constructor but yaml entry doesn't have type == insert" << endl;
+                exit(EXIT_FAILURE);
+            }
+        parseMap(insertdoc, node["insert"]);
+    }
+
+    // Execute the node
+    void insert::execute(mongocxx::client &conn) {
+        auto collection = conn["testdb"]["testCollection"];
+        auto result = collection.insert_one(insertdoc.view());
+        // need a way to exhaust the cursor 
+        cout << "insert is " << bsoncxx::to_json(insertdoc.view()) << endl;
+        // probably should do some error checking here
+    }
+}
