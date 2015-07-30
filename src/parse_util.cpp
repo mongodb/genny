1fd3ac2881ecdf39e4054dba8e0a896155896031
dalyd <david.daly@mongodb.com>
2015-07-29 21:35:16 -0400

    Sequences and Maps working now, except for nested sequences

--- src/parse_util.cpp
+++ src/parse_util.cpp
@@ -1,42 +1,58 @@
 #include "parse_util.hpp"
 
+using bsoncxx::builder::stream::open_document;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::array_context;
+using bsoncxx::builder::stream::single_context;
+
 namespace mwg {
 
 void parseMap(document &docbuilder, YAML::Node node) {
   for (auto entry : node) {
-    // cout << "In parseMap. entry.first: " << entry.first.Scalar()
-    //      << " entry.second" << entry.second.Scalar() << endl;
+      cout << "In parseMap. entry.first: " << entry.first.Scalar()
+          << " entry.second " << entry.second.Scalar() << endl;
       // can I just use basic document builder. Open, append, concatenate, etc? 
     if (entry.second.IsMap()) {
       docbuilder << entry.first.Scalar() << open_document;
       parseMap(docbuilder, entry.second);
       docbuilder << close_document;
     } else if (entry.second.IsSequence()) {
-        
-        parseSequence(docbuilder, entry.first.Scalar(), entry.second);
+        auto arraybuilder  = docbuilder << entry.first.Scalar() << open_array;
+        cout << "about to parseSequence" << endl;
+        parseSequence(arraybuilder, entry.second);
+        cout << "after to parseSequence" << endl;
+        arraybuilder << close_array;
+        cout << "closed array" << endl;
     } else { // scalar
       docbuilder << entry.first.Scalar() << entry.second.Scalar();
     }
   }
 }
     
-void parseSequence(document &docbuilder, string Name,
+    void parseSequence(bsoncxx::v0::builder::stream::array_context<bsoncxx::v0::builder::stream::key_context<bsoncxx::v0::builder::stream::closed_context>> &arraybuilder,
                        YAML::Node node) {
         
-        auto arraybuilder = docbuilder << Name << open_array;
         for (auto entry : node) {
-            if (entry.second.IsMap()) {
+            if (entry.IsMap()) {
+                cout << "Entry isMap" << endl;
                 document mydoc{};
-                parseMap(mydoc, entry.second);
+                parseMap(mydoc, entry);
                 bsoncxx::builder::stream::concatenate doc;
                 doc.view = mydoc.view();
-                arraybuilder << doc;
-            } else if (entry.second.IsSequence()) {
+                arraybuilder << open_document << doc << close_document;
+            } else if (entry.IsSequence()) {
+                cerr << "Nest sequences don't work yet" << endl;
+                exit(EXIT_FAILURE);
+                arraybuilder << open_array;
+                parseSequence(arraybuilder, entry);
             } else // scalar
                 {
-                    arraybuilder << entry.first.Scalar();
+                    cout << "Trying to put entry into array builder " << entry.Scalar() << endl;
+                    arraybuilder << entry.Scalar();
                 }
-            arraybuilder << close_array;
         }
     }
     
--- src/parse_util.hpp
+++ src/parse_util.hpp
@@ -5,17 +5,10 @@
 using namespace std;
 
 using bsoncxx::builder::stream::document;
-using bsoncxx::builder::stream::open_document;
-using bsoncxx::builder::stream::close_document;
-using bsoncxx::builder::stream::open_array;
-using bsoncxx::builder::stream::close_array;
-using bsoncxx::builder::stream::finalize;
-using bsoncxx::builder::stream::array_context;
-using bsoncxx::builder::stream::single_context;
 
 namespace mwg {
 
     void parseMap(document &, YAML::Node);
-    void parseSequence(document &, string, YAML::Node);
+    void parseSequence(bsoncxx::v0::builder::stream::array_context<bsoncxx::v0::builder::stream::key_context<bsoncxx::v0::builder::stream::closed_context>> &, YAML::Node);
 
 }
