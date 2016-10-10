46e7ee8961bf8ef05dd1bb792673bd6ef7f41542
dalyd <david.daly@mongodb.com>
2016-10-10 11:27:10 -0400

    Updated parse_results.py to handle nested workloads, and added stats for speed test

--- scripts/parse_results.py
+++ scripts/parse_results.py
@@ -21,42 +21,44 @@ def process_results_for_mc(filename):
     ''' Process the results and print out a table for mission control '''
 
     work, results = load_files(filename)
-    workload_name = results.keys()[0]
-    if not workload_name in work:
-        print ("Couldn't find workloadname {} from results in work yaml".format(workload_name))
-        sys.exit(1)
 
-    run_time = results[workload_name]['meanMicros']
     output = {'results': []}
-    for measure in work['results'][workload_name]:
-        name = measure['name']
-        count = 0
-        for node in measure['nodes']:
-            count += results[workload_name][node]['count']
+    for name, result_definition in work['results'].items():
+        workload_keys = name.split('.')
+        this_result = results
+        # Descend to the proper workload
+        for key in workload_keys:
+            this_result = this_result[key]
+        run_time = this_result['meanMicros']
+        for measure in result_definition:
+            name = measure['name']
+            count = 0
+            for node in measure['nodes']:
+                count += this_result[node]['count']
 
-        throughput = float(count * 1000 * 1000)/ run_time
-        print(">>> {0} : {1:12.2f} 1".format(name, throughput))
+                throughput = float(count * 1000 * 1000)/ run_time
+                print(">>> {0} : {1:12.2f} 1".format(name, throughput))
 
-        output['results'].append({'results': {'ops_per_sec': throughput},
-                                  'name': name,
-                                  'start': results[workload_name]['Date']})
-        # I think I can delete the following print statements. The
-        # one before here is the important one.
-        print("+--------------------------------+----------+--------------+----------+------------------------------+")
-        print(   "| {0:^30}".format("Test") +
-                 " | {0:^8}".format("Thread") +
-                 " | {0:^12}".format("Throughput") +
-                 " | {0:^8}".format("Pass?") +
-                 " | {0:^28}".format("Comment") +
-                 " | ")
-        print("|--------------------------------+----------+--------------+----------+------------------------------|")
-        print(   "| {0:^30}".format(name) +
-                 " | {0:^8}".format("1") +
-                 " | {0:12.2f}".format(throughput) +
-                 " | {0:^8}".format("true") +
-                 " | {0:^28}".format("") +
-                 " | ")
-        print("|--------------------------------+----------+--------------+----------+------------------------------|")
+                output['results'].append({'results': {'ops_per_sec': throughput},
+                                          'name': name,
+                                          'start': this_result['Date']})
+                # I think I can delete the following print statements. The
+                # one before here is the important one.
+                print("+--------------------------------+----------+--------------+----------+------------------------------+")
+                print(   "| {0:^30}".format("Test") +
+                         " | {0:^8}".format("Thread") +
+                         " | {0:^12}".format("Throughput") +
+                         " | {0:^8}".format("Pass?") +
+                         " | {0:^28}".format("Comment") +
+                         " | ")
+                print("|--------------------------------+----------+--------------+----------+------------------------------|")
+                print(   "| {0:^30}".format(name) +
+                         " | {0:^8}".format("1") +
+                         " | {0:12.2f}".format(throughput) +
+                         " | {0:^8}".format("true") +
+                         " | {0:^28}".format("") +
+                         " | ")
+                print("|--------------------------------+----------+--------------+----------+------------------------------|")
 
     with open('perf.json', 'w') as output_file:
         json.dump(output, output_file, indent=4, separators=(',', ': '))
