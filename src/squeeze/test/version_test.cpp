73af5979aaa53c27f2ab9b2695b8d3903ad739aa
Ryan Timmons <ryan.timmons@10gen.com>
2018-05-29 15:07:45 -0400

    tidy

--- src/squeeze/test/version_test.cpp
+++ src/squeeze/test/version_test.cpp
@@ -1,7 +1,6 @@
 #include "test.h"
 
-// TODO: how to make this <squeeze/version.h>
-#include <version.h>
+#include <squeeze/version.h>
 
 SCENARIO("We have the right version") {
     REQUIRE( squeeze::lib::get_version() == "0.0.1" );
