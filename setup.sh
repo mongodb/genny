815465d526453ed89e058323a62c97ea8f44bff9
Ryan Timmons <ryan.timmons@10gen.com>
2018-05-29 16:29:48 -0400

    download catch2.hpp for now

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ xcode-select --install
 brew install cmake
 brew install icu4c
 brew install boost --build-from-source --include-test --with-icu4c
+
+if [ ! -e "src/third_party/catch2/include/catch.hpp" ]; then
+    curl -fsS "https://raw.githubusercontent.com/CatchOrg/Catch2/master/single_include/catch.hpp" \
+        > "src/third_party/catch2/include/catch.hpp"
+fi
