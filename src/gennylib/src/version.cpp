4d17bf98f91b0ec815799967d575c8174c3074af
Ryan Timmons <ryan.timmons@10gen.com>
2018-05-31 16:39:49 -0400

    Comments around version numbers and the cmake export

--- src/gennylib/src/version.cpp
+++ src/gennylib/src/version.cpp
@@ -3,6 +3,8 @@
 
 std::string genny::lib::get_version() {
     BOOST_LOG_TRIVIAL(info) << "Confirmed logging through boost";
+    // NB: this version number is duplicated in
+    // src/CMakeList.txt, src/gennylib/CMakeLists.txt and src/gennylib/src/version.cpp
     return "0.0.1";
 }
 
