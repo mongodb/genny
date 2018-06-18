20a694c85c5e4705be3f1cfe959ee9fda3ce217c
Ryan Timmons <ryan.timmons@10gen.com>
2018-06-18 14:49:23 -0400

    Remove diff-cruft

--- src/driver/src/main.cpp
+++ src/driver/src/main.cpp
@@ -1,5 +1,5 @@
-#include <experimental/optional>
 #include <iostream>
+#include <optional>
 
 #include <gennylib/metrics.hpp>
 #include <gennylib/version.hpp>
@@ -9,7 +9,7 @@
 
 int main(int argc, char** argv) {
     // basically just a test that we're using c++17
-    auto v = std::experimental::make_optional(genny::getVersion());
+    auto v = std::make_optional(genny::getVersion());
     std::cout << u8"🧞 Genny" << " Version " << v.value_or("ERROR") << u8" 💝🐹🌇⛔" << std::endl;
 
     genny::driver::DefaultDriver d;
