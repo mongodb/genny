b0ef8a886855ddcc51c07f313ea0f38f3763d08a
Ryan Timmons <ryan.timmons@10gen.com>
2018-06-18 13:50:06 -0400

    Don't need experimental now

--- src/driver/src/main.cpp
+++ src/driver/src/main.cpp
@@ -1,13 +1,12 @@
-#include <experimental/optional>
-
 #include <iostream>
+#include <optional>
 #include <string>
 
 #include <gennylib/version.hpp>
 
 int main() {
     // basically just a test that we're using c++17
-    auto v { std::experimental::make_optional(genny::getVersion()) };
+    auto v { std::make_optional(genny::getVersion()) };
     std::cout << u8"🧞 Genny" << " Version " << v.value_or("ERROR") << u8" 💝🐹🌇⛔" << std::endl;
     return 0;
 }
