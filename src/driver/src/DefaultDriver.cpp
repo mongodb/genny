4f7b22823db794dccc23164a0df183a132400de6
julianedwards <julian.edwards@mongodb.com>
2018-07-16 11:34:46 -0400

    add some metrics

--- src/driver/src/DefaultDriver.cpp
+++ src/driver/src/DefaultDriver.cpp
@@ -15,16 +15,22 @@ int genny::driver::DefaultDriver::run(int, char**) const {
     genny::metrics::Registry metrics;
     auto orchestrator = Orchestrator{nActors};
 
+    auto actorSetupTimer = metrics.timer("actorSetup");
+    auto stopwatch = actorSetupTimer.start();
     std::vector<std::unique_ptr<genny::PhasedActor>> actors;
     actors.push_back(std::make_unique<genny::actor::HelloWorld>(orchestrator, metrics, "one"));
     actors.push_back(std::make_unique<genny::actor::HelloWorld>(orchestrator, metrics, "two"));
+    stopwatch.report();
 
     assert(actors.size() == nActors);
 
+    std::mutex lock;
+    auto threadCounter = metrics.counter("threadCounter");
     std::vector<std::thread> threads;
     std::transform(
-        cbegin(actors), cend(actors), std::back_inserter(threads), [](const auto& actor) {
-            return std::thread{&genny::PhasedActor::run, actor.get()};
+        cbegin(actors), cend(actors), std::back_inserter(threads), [&](const auto& actor) {
+            return std::thread{
+                [&]() {lock.lock(); threadCounter.incr(); lock.unlock(); actor->run(); lock.lock(); threadCounter.decr(); lock.unlock();}};
         });
 
     for (auto& thread : threads)
