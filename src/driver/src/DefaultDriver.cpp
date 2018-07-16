82a6bbeed3cbc9ae0af6a3d5e0653c44d8a6f4ef
julianedwards <julian.edwards@mongodb.com>
2018-07-16 11:37:38 -0400

    formatting

--- src/driver/src/DefaultDriver.cpp
+++ src/driver/src/DefaultDriver.cpp
@@ -9,35 +9,44 @@
 
 #include "DefaultDriver.hpp"
 
-int genny::driver::DefaultDriver::run(int, char**) const {
-    const int nActors = 2;
+int genny::driver::DefaultDriver::run(int, char **) const {
+  const int nActors = 2;
 
-    genny::metrics::Registry metrics;
-    auto orchestrator = Orchestrator{nActors};
+  genny::metrics::Registry metrics;
+  auto orchestrator = Orchestrator{nActors};
 
-    auto actorSetupTimer = metrics.timer("actorSetup");
-    auto stopwatch = actorSetupTimer.start();
-    std::vector<std::unique_ptr<genny::PhasedActor>> actors;
-    actors.push_back(std::make_unique<genny::actor::HelloWorld>(orchestrator, metrics, "one"));
-    actors.push_back(std::make_unique<genny::actor::HelloWorld>(orchestrator, metrics, "two"));
-    stopwatch.report();
+  auto actorSetupTimer = metrics.timer("actorSetup");
+  auto stopwatch = actorSetupTimer.start();
+  std::vector<std::unique_ptr<genny::PhasedActor>> actors;
+  actors.push_back(
+      std::make_unique<genny::actor::HelloWorld>(orchestrator, metrics, "one"));
+  actors.push_back(
+      std::make_unique<genny::actor::HelloWorld>(orchestrator, metrics, "two"));
+  stopwatch.report();
 
-    assert(actors.size() == nActors);
+  assert(actors.size() == nActors);
 
-    std::mutex lock;
-    auto threadCounter = metrics.counter("threadCounter");
-    std::vector<std::thread> threads;
-    std::transform(
-        cbegin(actors), cend(actors), std::back_inserter(threads), [&](const auto& actor) {
-            return std::thread{
-                [&]() {lock.lock(); threadCounter.incr(); lock.unlock(); actor->run(); lock.lock(); threadCounter.decr(); lock.unlock();}};
-        });
+  std::mutex lock;
+  auto threadCounter = metrics.counter("threadCounter");
+  std::vector<std::thread> threads;
+  std::transform(cbegin(actors), cend(actors), std::back_inserter(threads),
+                 [&](const auto &actor) {
+                   return std::thread{[&]() {
+                     lock.lock();
+                     threadCounter.incr();
+                     lock.unlock();
+                     actor->run();
+                     lock.lock();
+                     threadCounter.decr();
+                     lock.unlock();
+                   }};
+                 });
 
-    for (auto& thread : threads)
-        thread.join();
+  for (auto &thread : threads)
+    thread.join();
 
-    const auto reporter = genny::metrics::Reporter{metrics};
-    reporter.report(std::cout);
+  const auto reporter = genny::metrics::Reporter{metrics};
+  reporter.report(std::cout);
 
-    return 0;
+  return 0;
 }
