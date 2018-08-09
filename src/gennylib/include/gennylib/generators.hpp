417d18ee449a6249fcadddb75397a11fe9892af7
dalyd <david.daly@mongodb.com>
2018-08-09 08:15:34 -0400

    Copied a bunch of generator stuff over. Compiling but not linking

--- src/gennylib/include/gennylib/generators.hpp
+++ src/gennylib/include/gennylib/generators.hpp
@@ -3,51 +3,54 @@
 
 #include <bsoncxx/builder/stream/document.hpp>
 #include <optional>
+#include <random>
+#include <unordered_map>
+
+#include "parse_util.hpp"
 
 namespace genny {
 
+class ValueGenerator;
+
 class document {
 public:
-    virtual ~document() = 0;
-    virtual bsoncxx::document::view view(bsoncxx::builder::stream::document& doc, threadState&) {
-        return doc.view();
-    };
+  virtual ~document() = 0;
+  virtual bsoncxx::document::view view(bsoncxx::builder::stream::document &doc,
+                                       std::mt19937_64 &) {
+    return doc.view();
+  };
 };
 
 class bsonDocument : public document {
 public:
-    bsonDocument();
-    bsonDocument(YAML::Node&);
+  bsonDocument();
+  bsonDocument(YAML::Node &);
 
-    void setDoc(bsoncxx::document::value value) {
-        doc = value;
-    }
-    virtual bsoncxx::document::view view(bsoncxx::builder::stream::document&,
-                                         threadState&) override;
+  void setDoc(bsoncxx::document::value value) { doc = value; }
+  virtual bsoncxx::document::view view(bsoncxx::builder::stream::document &,
+                                       std::mt19937_64 &) override;
 
 private:
-    std::optional<bsoncxx::document::value> doc;
+  std::optional<bsoncxx::document::value> doc;
 };
 
 class templateDocument : public document {
 public:
-    templateDocument();
-    templateDocument(YAML::Node&);
-    virtual bsoncxx::document::view view(bsoncxx::builder::stream::document&) override;
+  templateDocument();
+  templateDocument(YAML::Node &);
+  virtual bsoncxx::document::view view(bsoncxx::builder::stream::document &,
+                                       std::mt19937_64 &) override;
 
 protected:
-    // The document to override
-    bsonDocument doc;
-    unordered_map<string, unique_ptr<ValueGenerator>> override;
+  // The document to override
+  bsonDocument doc;
+  unordered_map<string, unique_ptr<ValueGenerator>> override;
 
 private:
-    // apply the overides, one level at a time
-    void applyOverrideLevel(bsoncxx::builder::stream::document&,
-                            bsoncxx::document::view,
-                            string,
-                            threadState&);
+  // apply the overides, one level at a time
+  void applyOverrideLevel(bsoncxx::builder::stream::document &,
+                          bsoncxx::document::view, string, std::mt19937_64 &);
 };
-}  // namespace genny
+} // namespace genny
 
-
-#endif  // HEADER_E6E05F14_BE21_4A9B_822D_FFD669CFB1B4_INCLUDED
+#endif // HEADER_E6E05F14_BE21_4A9B_822D_FFD669CFB1B4_INCLUDED
--- src/gennylib/src/generators.cpp
+++ src/gennylib/src/generators.cpp
@@ -2,28 +2,31 @@
 
 #include <boost/log/trivial.hpp>
 #include <bsoncxx/json.hpp>
+#include <random>
 #include <stdlib.h>
 
 namespace genny {
 
 bsonDocument::bsonDocument() {
-    doc = bsoncxx::builder::stream::document{} << finalize;
+  doc = bsoncxx::builder::stream::document{}
+        << bsoncxx::builder::stream::finalize;
 }
 
-bsonDocument::bsonDocument(YAML::Node& node) {
-    if (!node) {
-        BOOST_LOG_TRIVIAL(info) << "bsonDocument constructor using empty document";
-    } else if (!node.IsMap()) {
-        BOOST_LOG_TRIVIAL(fatal) << "Not map in bsonDocument constructor";
-        exit(EXIT_FAILURE);
-    } else {
-        BOOST_LOG_TRIVIAL(trace) << "In bsonDocument constructor";
-        doc = parseMap(node);
-        BOOST_LOG_TRIVIAL(trace) << "Parsed map in bsonDocument constructor";
-    }
+bsonDocument::bsonDocument(YAML::Node &node) {
+  if (!node) {
+    BOOST_LOG_TRIVIAL(info) << "bsonDocument constructor using empty document";
+  } else if (!node.IsMap()) {
+    BOOST_LOG_TRIVIAL(fatal) << "Not map in bsonDocument constructor";
+    exit(EXIT_FAILURE);
+  } else {
+    BOOST_LOG_TRIVIAL(trace) << "In bsonDocument constructor";
+    doc = parseMap(node);
+    BOOST_LOG_TRIVIAL(trace) << "Parsed map in bsonDocument constructor";
+  }
 }
 
-bsoncxx::document::view bsonDocument::view(bsoncxx::builder::stream::document&, threadState&) {
-    return doc->view();
+bsoncxx::document::view bsonDocument::view(bsoncxx::builder::stream::document &,
+                                           std::mt19937_64 &) {
+  return doc->view();
 }
-}  // namespace genny
+} // namespace genny
