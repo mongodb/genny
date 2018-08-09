d569a87d68843101825427991138f0f6d08c306e
dalyd <david.daly@mongodb.com>
2018-08-09 14:02:20 -0400

    Working with insert example for static data

--- src/gennylib/src/generators.cpp
+++ src/gennylib/src/generators.cpp
@@ -8,34 +8,32 @@
 namespace genny {
 
 bsonDocument::bsonDocument() {
-  doc = bsoncxx::builder::stream::document{}
-        << bsoncxx::builder::stream::finalize;
+    doc = bsoncxx::builder::stream::document{} << bsoncxx::builder::stream::finalize;
 }
 
 bsonDocument::bsonDocument(const YAML::Node node) {
-  if (!node) {
-    BOOST_LOG_TRIVIAL(info) << "bsonDocument constructor using empty document";
-  } else if (!node.IsMap()) {
-    BOOST_LOG_TRIVIAL(fatal) << "Not map in bsonDocument constructor";
-    exit(EXIT_FAILURE);
-  } else {
-    BOOST_LOG_TRIVIAL(trace) << "In bsonDocument constructor";
-    doc = parseMap(node);
-    BOOST_LOG_TRIVIAL(trace) << "Parsed map in bsonDocument constructor";
-  }
+    if (!node) {
+        BOOST_LOG_TRIVIAL(info) << "bsonDocument constructor using empty document";
+    } else if (!node.IsMap()) {
+        BOOST_LOG_TRIVIAL(fatal) << "Not map in bsonDocument constructor";
+        exit(EXIT_FAILURE);
+    } else {
+        BOOST_LOG_TRIVIAL(trace) << "In bsonDocument constructor";
+        doc = parseMap(node);
+        BOOST_LOG_TRIVIAL(trace) << "Parsed map in bsonDocument constructor";
+    }
 }
 
-bsoncxx::document::view bsonDocument::view(bsoncxx::builder::stream::document &,
-                                           std::mt19937_64 &) {
-  return doc->view();
+bsoncxx::document::view bsonDocument::view(bsoncxx::builder::stream::document&, std::mt19937_64&) {
+    return doc->view();
 }
 
 // parse a YAML Node and make a document of the correct type
 unique_ptr<document> makeDoc(const YAML::Node node) {
-  if (!node) { // empty document should be bsonDocument
-    return unique_ptr<document>{new bsonDocument(node)};
-  } else if (!node["type"] or node["type"].Scalar() == "bson") {
-    return unique_ptr<document>{new bsonDocument(node)};
+    if (!node) {  // empty document should be bsonDocument
+        return unique_ptr<document>{new bsonDocument(node)};
+    } else  // if (!node["type"] or node["type"].Scalar() == "bson") {
+        return unique_ptr<document>{new bsonDocument(node)};
     // } else if (!node["type"] or node["type"].Scalar() == "templating") {
     //   return unique_ptr<document>{new templateDocument(node)};
     // } else if (node["type"].Scalar() == "bson") {
@@ -44,10 +42,10 @@ unique_ptr<document> makeDoc(const YAML::Node node) {
     //   return unique_ptr<document>{new overrideDocument(node)};
     // } else if (node["type"].Scalar() == "append") {
     //   return unique_ptr<document>{new AppendDocument(node)};
-  } else {
-    BOOST_LOG_TRIVIAL(fatal)
-        << "in makeDoc and type exists, and isn't bson or override";
-    exit(EXIT_FAILURE);
-  }
+    // } else {
+    //   BOOST_LOG_TRIVIAL(fatal)
+    //       << "in makeDoc and type exists, and isn't bson or override";
+    //   exit(EXIT_FAILURE);
+    // }
 };
-} // namespace genny
+}  // namespace genny
