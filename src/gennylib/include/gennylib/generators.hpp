d0d214fb35a4c98e28eceea17d0c91d3b4681232
dalyd <david.daly@mongodb.com>
2018-08-09 14:24:09 -0400

    Copied a lot over

--- src/gennylib/include/gennylib/generators.hpp
+++ src/gennylib/include/gennylib/generators.hpp
@@ -8,53 +8,135 @@
 
 #include "parse_util.hpp"
 
+using view_or_value = bsoncxx::view_or_value<bsoncxx::array::view, bsoncxx::array::value>;
+
 namespace genny {
 
-class ValueGenerator;
+// Generate a value, such as a random value or access a variable
+class ValueGenerator {
+public:
+    ValueGenerator(const YAML::Node&){};
+    virtual ~ValueGenerator(){};
+    // Generate a new value.
+    virtual bsoncxx::array::value generate(std::mt19937_64&) = 0;
+    // Need some helper functions here to get a string, or an int
+    virtual int64_t generateInt(std::mt19937_64&);
+    virtual double generateDouble(std::mt19937_64&);
+    virtual std::string generateString(std::mt19937_64&);
+};
+
+const std::set<std::string> getGeneratorTypes();
+std::unique_ptr<ValueGenerator> makeUniqueValueGenerator(YAML::Node);
+std::shared_ptr<ValueGenerator> makeSharedValueGenerator(YAML::Node);
+std::unique_ptr<ValueGenerator> makeUniqueValueGenerator(YAML::Node, std::string);
+std::shared_ptr<ValueGenerator> makeSharedValueGenerator(YAML::Node, std::string);
+std::string valAsString(view_or_value);
+int64_t valAsInt(view_or_value);
+double valAsDouble(view_or_value);
+
+class UseValueGenerator : public ValueGenerator {
+public:
+    UseValueGenerator(YAML::Node&);
+    virtual bsoncxx::array::value generate(std::mt19937_64&) override;
+
+private:
+    optional<bsoncxx::array::value> value;
+};
+
+// Class to wrap either a plain int64_t, or a value generator that will be called as an int. This
+// can be templatized if there are enough variants
+class IntOrValue {
+public:
+    IntOrValue() : myInt(0), myGenerator(nullptr), isInt(true) {}
+    IntOrValue(int64_t inInt) : myInt(inInt), myGenerator(nullptr), isInt(true) {}
+    IntOrValue(std::unique_ptr<ValueGenerator> generator)
+        : myInt(0), myGenerator(std::move(generator)), isInt(false) {}
+    IntOrValue(YAML::Node);
+
+    int64_t getInt(std::mt19937_64& state) {
+        if (isInt) {
+            return (myInt);
+        } else
+            return myGenerator->generateInt(state);
+    }
+
+
+private:
+    int64_t myInt;
+    std::unique_ptr<ValueGenerator> myGenerator;
+    bool isInt;
+};
+enum class GeneratorType {
+    UNIFORM,
+    BINOMIAL,
+    NEGATIVE_BINOMIAL,
+    GEOMETRIC,
+    POISSON,
+};
+
+class RandomIntGenerator : public ValueGenerator {
+public:
+    RandomIntGenerator(const YAML::Node&);
+    virtual bsoncxx::array::value generate(std::mt19937_64&) override;
+    virtual int64_t generateInt(std::mt19937_64&) override;
+    virtual std::string generateString(std::mt19937_64&) override;
+
+private:
+    GeneratorType generator;
+    IntOrValue min;
+    IntOrValue max;
+    IntOrValue t;                          // for binomial, negative binomial
+    std::unique_ptr<ValueGenerator> p;     // for binomial, geometric
+    std::unique_ptr<ValueGenerator> mean;  // for poisson
+};
 
 class document {
 public:
-  virtual ~document(){};
-  virtual bsoncxx::document::view view(bsoncxx::builder::stream::document &doc,
-                                       std::mt19937_64 &) {
-    return doc.view();
-  };
+    virtual ~document(){};
+    virtual bsoncxx::document::view view(bsoncxx::builder::stream::document& doc,
+                                         std::mt19937_64&) {
+        return doc.view();
+    };
 };
 
 class bsonDocument : public document {
 public:
-  bsonDocument();
-  bsonDocument(const YAML::Node);
+    bsonDocument();
+    bsonDocument(const YAML::Node);
 
-  void setDoc(bsoncxx::document::value value) { doc = value; }
-  virtual bsoncxx::document::view view(bsoncxx::builder::stream::document &,
-                                       std::mt19937_64 &) override;
+    void setDoc(bsoncxx::document::value value) {
+        doc = value;
+    }
+    virtual bsoncxx::document::view view(bsoncxx::builder::stream::document&,
+                                         std::mt19937_64&) override;
 
 private:
-  std::optional<bsoncxx::document::value> doc;
+    std::optional<bsoncxx::document::value> doc;
 };
 
 class templateDocument : public document {
 public:
-  templateDocument();
-  templateDocument(const YAML::Node);
-  virtual bsoncxx::document::view view(bsoncxx::builder::stream::document &,
-                                       std::mt19937_64 &) override;
+    templateDocument();
+    templateDocument(const YAML::Node);
+    virtual bsoncxx::document::view view(bsoncxx::builder::stream::document&,
+                                         std::mt19937_64&) override;
 
 protected:
-  // The document to override
-  bsonDocument doc;
-  unordered_map<string, unique_ptr<ValueGenerator>> override;
+    // The document to override
+    bsonDocument doc;
+    unordered_map<string, unique_ptr<ValueGenerator>> override;
 
 private:
-  // apply the overides, one level at a time
-  void applyOverrideLevel(bsoncxx::builder::stream::document &,
-                          bsoncxx::document::view, string, std::mt19937_64 &);
+    // apply the overides, one level at a time
+    void applyOverrideLevel(bsoncxx::builder::stream::document&,
+                            bsoncxx::document::view,
+                            string,
+                            std::mt19937_64&);
 };
 
 // parse a YAML Node and make a document of the correct type
 unique_ptr<document> makeDoc(const YAML::Node);
 
-} // namespace genny
+}  // namespace genny
 
-#endif // HEADER_E6E05F14_BE21_4A9B_822D_FFD669CFB1B4_INCLUDED
+#endif  // HEADER_E6E05F14_BE21_4A9B_822D_FFD669CFB1B4_INCLUDED
--- src/gennylib/src/generators.cpp
+++ src/gennylib/src/generators.cpp
@@ -5,6 +5,8 @@
 #include <random>
 #include <stdlib.h>
 
+using bsoncxx::builder::stream::finalize;
+
 namespace genny {
 
 bsonDocument::bsonDocument() {
@@ -48,4 +50,365 @@ unique_ptr<document> makeDoc(const YAML::Node node) {
     //   exit(EXIT_FAILURE);
     // }
 };
+
+// This returns a set of the value generator types with $ prefixes
+const std::set<std::string> getGeneratorTypes() {
+    return (std::set<std::string>{});  // "$add",
+                                       // "$choose",
+                                       // "$concatenate",
+                                       // "$date",
+                                       // "$increment",
+                                       // "$multiply",
+                                       // "$randomint",
+                                       // "$fastrandomstring",
+                                       // "$randomstring",
+                                       // "$useresult",
+                                       // "$useval",
+                                       // "$usevar"});
+}
+
+
+ValueGenerator* makeValueGenerator(YAML::Node yamlNode, std::string type) {
+    // if (type == "add") {
+    //     return new AddGenerator(yamlNode);
+    // } else if (type == "choose") {
+    //     return new ChooseGenerator(yamlNode);
+    // } else if (type == "concatenate") {
+    //     return new ConcatenateGenerator(yamlNode);
+    // } else if (type == "date") {
+    //     return new DateGenerator(yamlNode);
+    // } else if (type == "increment") {
+    //     return new IncrementGenerator(yamlNode);
+    // } else if (type == "multiply") {
+    //     return new MultiplyGenerator(yamlNode);
+    // } else if (type == "randomint") {
+    //     return new RandomIntGenerator(yamlNode);
+    // } else if (type == "randomstring") {
+    //     return new RandomStringGenerator(yamlNode);
+    // } else if (type == "fastrandomstring") {
+    //     return new FastRandomStringGenerator(yamlNode);
+    // } else if (type == "useresult") {
+    //     return new UseResultGenerator(yamlNode);
+    // } else if (type == "useval") {
+    //     return new UseValueGenerator(yamlNode);
+    // } else if (type == "usevar") {
+    //     return new UseVarGenerator(yamlNode);
+    // }
+    BOOST_LOG_TRIVIAL(fatal) << "In makeValueGenerator and don't know how to handle type " << type;
+    exit(EXIT_FAILURE);
+}
+
+ValueGenerator* makeValueGenerator(YAML::Node yamlNode) {
+    if (yamlNode.IsScalar()) {
+        return new UseValueGenerator(yamlNode);
+    }
+    // Should we put a list directly into UseValueGenerator also?
+    if (!yamlNode.IsMap()) {
+        BOOST_LOG_TRIVIAL(fatal)
+            << "ValueGenerator Node in makeValueGenerator is not a yaml map or a sequence";
+        exit(EXIT_FAILURE);
+    }
+    if (auto type = yamlNode["type"])
+        return (makeValueGenerator(yamlNode, type.Scalar()));
+    // If it doesn't have a type field, search for templating keys
+    for (auto&& entry : yamlNode) {
+        auto key = entry.first.Scalar();
+        if (getGeneratorTypes().count(key)) {
+            auto type = key.substr(1, key.length());
+            return (makeValueGenerator(entry.second, type));
+        }
+    }
+    return (makeValueGenerator(yamlNode, "useval"));
+}
+
+int64_t ValueGenerator::generateInt(std::mt19937_64& state) {
+    return valAsInt(generate(state));
+};
+double ValueGenerator::generateDouble(std::mt19937_64& state) {
+    return valAsDouble(generate(state));
+};
+std::string ValueGenerator::generateString(std::mt19937_64& state) {
+    return valAsString(generate(state));
+}
+
+
+std::unique_ptr<ValueGenerator> makeUniqueValueGenerator(YAML::Node yamlNode) {
+    return std::unique_ptr<ValueGenerator>(makeValueGenerator(yamlNode));
+}
+std::shared_ptr<ValueGenerator> makeSharedValueGenerator(YAML::Node yamlNode) {
+    return std::unique_ptr<ValueGenerator>(makeValueGenerator(yamlNode));
+}
+
+std::unique_ptr<ValueGenerator> makeUniqueValueGenerator(YAML::Node yamlNode, std::string type) {
+    return std::unique_ptr<ValueGenerator>(makeValueGenerator(yamlNode, type));
+}
+std::shared_ptr<ValueGenerator> makeSharedValueGenerator(YAML::Node yamlNode, std::string type) {
+    return std::unique_ptr<ValueGenerator>(makeValueGenerator(yamlNode, type));
+}
+
+// Check type cases and get a string out of it. Assumes it is getting a bson array of length 1.
+std::string valAsString(view_or_value val) {
+    auto elem = val.view()[0];
+    switch (elem.type()) {
+        case bsoncxx::type::k_int64:
+            return (std::to_string(elem.get_int64().value));
+            break;
+        case bsoncxx::type::k_int32:
+            return (std::to_string(elem.get_int32().value));
+            break;
+        case bsoncxx::type::k_double:
+            return (std::to_string(elem.get_double().value));
+            break;
+        case bsoncxx::type::k_utf8:
+            return (elem.get_utf8().value.to_string());
+            break;
+        case bsoncxx::type::k_document:
+        case bsoncxx::type::k_array:
+        case bsoncxx::type::k_binary:
+        case bsoncxx::type::k_undefined:
+        case bsoncxx::type::k_oid:
+        case bsoncxx::type::k_bool:
+        case bsoncxx::type::k_date:
+        case bsoncxx::type::k_null:
+        case bsoncxx::type::k_regex:
+        case bsoncxx::type::k_dbpointer:
+        case bsoncxx::type::k_code:
+        case bsoncxx::type::k_symbol:
+        case bsoncxx::type::k_timestamp:
+
+            BOOST_LOG_TRIVIAL(fatal) << "valAsString with type unsuported type in list";
+            exit(EXIT_FAILURE);
+            break;
+        default:
+            BOOST_LOG_TRIVIAL(fatal) << "valAsString with type unsuported type not in list";
+            exit(EXIT_FAILURE);
+    }
+    return ("");
+}
+int64_t valAsInt(view_or_value val) {
+    auto elem = val.view()[0];
+    switch (elem.type()) {
+        case bsoncxx::type::k_int64:
+            return (elem.get_int64().value);
+            break;
+        case bsoncxx::type::k_int32:
+            return (static_cast<int64_t>(elem.get_int32().value));
+            break;
+        case bsoncxx::type::k_double:
+            return (static_cast<int64_t>(elem.get_double().value));
+            break;
+        case bsoncxx::type::k_utf8:
+        case bsoncxx::type::k_document:
+        case bsoncxx::type::k_array:
+        case bsoncxx::type::k_binary:
+        case bsoncxx::type::k_undefined:
+        case bsoncxx::type::k_oid:
+        case bsoncxx::type::k_bool:
+        case bsoncxx::type::k_date:
+        case bsoncxx::type::k_null:
+        case bsoncxx::type::k_regex:
+        case bsoncxx::type::k_dbpointer:
+        case bsoncxx::type::k_code:
+        case bsoncxx::type::k_symbol:
+        case bsoncxx::type::k_timestamp:
+
+            BOOST_LOG_TRIVIAL(fatal) << "valAsInt with type unsuported type in list";
+            exit(EXIT_FAILURE);
+            break;
+        default:
+            BOOST_LOG_TRIVIAL(fatal) << "valAsInt with type unsuported type not in list";
+            exit(EXIT_FAILURE);
+    }
+    return (0);
+}
+double valAsDouble(view_or_value val) {
+    auto elem = val.view()[0];
+    switch (elem.type()) {
+        case bsoncxx::type::k_int64:
+            return (static_cast<double>(elem.get_int64().value));
+            break;
+        case bsoncxx::type::k_int32:
+            return (static_cast<double>(elem.get_int32().value));
+            break;
+        case bsoncxx::type::k_double:
+            return (static_cast<double>(elem.get_double().value));
+            break;
+        case bsoncxx::type::k_utf8:
+        case bsoncxx::type::k_document:
+        case bsoncxx::type::k_array:
+        case bsoncxx::type::k_binary:
+        case bsoncxx::type::k_undefined:
+        case bsoncxx::type::k_oid:
+        case bsoncxx::type::k_bool:
+        case bsoncxx::type::k_date:
+        case bsoncxx::type::k_null:
+        case bsoncxx::type::k_regex:
+        case bsoncxx::type::k_dbpointer:
+        case bsoncxx::type::k_code:
+        case bsoncxx::type::k_symbol:
+        case bsoncxx::type::k_timestamp:
+
+            BOOST_LOG_TRIVIAL(fatal) << "valAsInt with type unsuported type in list";
+            exit(EXIT_FAILURE);
+            break;
+        default:
+            BOOST_LOG_TRIVIAL(fatal) << "valAsInt with type unsuported type not in list";
+            exit(EXIT_FAILURE);
+    }
+    return (0);
+}
+
+UseValueGenerator::UseValueGenerator(YAML::Node& node) : ValueGenerator(node) {
+    // add in error checking
+    if (node.IsScalar()) {
+        value = yamlToValue(node);
+    } else {
+        value = yamlToValue(node["value"]);
+    }
+}
+
+bsoncxx::array::value UseValueGenerator::generate(std::mt19937_64& state) {
+    // probably should actually return a view or a copy of the value
+    return (bsoncxx::array::value(*value));
+}
+RandomIntGenerator::RandomIntGenerator(const YAML::Node& node)
+    : ValueGenerator(node), generator(GeneratorType::UNIFORM), min(0), max(100), t(10) {
+    // It's okay to have a scalar for the templating. Just use defaults
+    if (node.IsMap()) {
+        if (auto distributionNode = node["distribution"]) {
+            auto distributionString = distributionNode.Scalar();
+            if (distributionString == "uniform")
+                generator = GeneratorType::UNIFORM;
+            else if (distributionString == "binomial")
+                generator = GeneratorType::BINOMIAL;
+            else if (distributionString == "negative_binomial")
+                generator = GeneratorType::NEGATIVE_BINOMIAL;
+            else if (distributionString == "geometric")
+                generator = GeneratorType::GEOMETRIC;
+            else if (distributionString == "poisson")
+                generator = GeneratorType::POISSON;
+            else {
+                BOOST_LOG_TRIVIAL(fatal)
+                    << "In RandomIntGenerator and have unknown distribution type "
+                    << distributionString;
+                exit(EXIT_FAILURE);
+            }
+        }
+        // now read in parameters based on the distribution type
+        switch (generator) {
+            case GeneratorType::UNIFORM:
+                if (auto minimum = node["min"]) {
+                    min = IntOrValue(minimum);
+                }
+                if (auto maximum = node["max"]) {
+                    max = IntOrValue(maximum);
+                }
+                break;
+            case GeneratorType::BINOMIAL:
+                if (auto trials = node["t"])
+                    t = IntOrValue(trials);
+                else
+                    BOOST_LOG_TRIVIAL(warning)
+                        << "Binomial distribution in random int, but no t parameter";
+                if (auto probability = node["p"])
+                    p = makeUniqueValueGenerator(probability);
+                else {
+                    BOOST_LOG_TRIVIAL(fatal)
+                        << "Binomial distribution in random int, but no p parameter";
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case GeneratorType::NEGATIVE_BINOMIAL:
+                if (auto kval = node["k"])
+                    t = IntOrValue(kval);
+                else
+                    BOOST_LOG_TRIVIAL(warning)
+                        << "Negative binomial distribution in random int, not no k parameter";
+                if (auto probability = node["p"])
+                    p = makeUniqueValueGenerator(probability);
+                else {
+                    BOOST_LOG_TRIVIAL(fatal)
+                        << "Binomial distribution in random int, but no p parameter";
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case GeneratorType::GEOMETRIC:
+                if (auto probability = node["p"])
+                    p = makeUniqueValueGenerator(probability);
+                else {
+                    BOOST_LOG_TRIVIAL(fatal)
+                        << "Geometric distribution in random int, but no p parameter";
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case GeneratorType::POISSON:
+                if (auto meannode = node["mean"])
+                    mean = makeUniqueValueGenerator(meannode);
+                else {
+                    BOOST_LOG_TRIVIAL(fatal)
+                        << "Geometric distribution in random int, but no p parameter";
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            default:
+                BOOST_LOG_TRIVIAL(fatal)
+                    << "Unknown generator type in RandomIntGenerator in switch statement";
+                exit(EXIT_FAILURE);
+                break;
+        }
+    }
+}
+
+int64_t RandomIntGenerator::generateInt(std::mt19937_64& rng) {
+    switch (generator) {
+        case GeneratorType::UNIFORM: {
+            uniform_int_distribution<int64_t> distribution(min.getInt(rng), max.getInt(rng));
+            return (distribution(rng));
+        } break;
+        case GeneratorType::BINOMIAL: {
+            binomial_distribution<int64_t> distribution(t.getInt(rng), p->generateDouble(rng));
+            return (distribution(rng));
+        } break;
+        case GeneratorType::NEGATIVE_BINOMIAL: {
+            negative_binomial_distribution<int64_t> distribution(t.getInt(rng),
+                                                                 p->generateDouble(rng));
+            return (distribution(rng));
+        } break;
+        case GeneratorType::GEOMETRIC: {
+            geometric_distribution<int64_t> distribution(p->generateDouble(rng));
+            return (distribution(rng));
+        } break;
+        case GeneratorType::POISSON: {
+            poisson_distribution<int64_t> distribution(mean->generateDouble(rng));
+            return (distribution(rng));
+        } break;
+        default:
+            BOOST_LOG_TRIVIAL(fatal)
+                << "Unknown generator type in RandomIntGenerator in switch rngment";
+            exit(EXIT_FAILURE);
+            break;
+    }
+    BOOST_LOG_TRIVIAL(fatal)
+        << "Reached end of RandomIntGenerator::generateInt. Should have returned earlier";
+    exit(EXIT_FAILURE);
+}
+std::string RandomIntGenerator::generateString(std::mt19937_64& rng) {
+    return (std::to_string(generateInt(rng)));
+}
+bsoncxx::array::value RandomIntGenerator::generate(std::mt19937_64& rng) {
+    return (bsoncxx::builder::stream::array{} << generateInt(rng)
+                                              << bsoncxx::builder::stream::finalize);
+}
+
+IntOrValue::IntOrValue(YAML::Node yamlNode) : myInt(0), myGenerator(nullptr) {
+    if (yamlNode.IsScalar()) {
+        // Read in just a number
+        isInt = true;
+        myInt = yamlNode.as<int64_t>();
+    } else {
+        // use a value generator
+        isInt = false;
+        myGenerator = makeUniqueValueGenerator(yamlNode);
+    }
+}
 }  // namespace genny
