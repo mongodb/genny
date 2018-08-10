e3158049535e9666bb9f49a65651af7683f667e7
dalyd <david.daly@mongodb.com>
2018-08-10 11:30:33 -0400

    Set default logging level to info

--- src/driver/src/main.cpp
+++ src/driver/src/main.cpp
@@ -1,3 +1,6 @@
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/trivial.hpp>
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -15,6 +18,7 @@ int main(int argc, char** argv) {
     // basically just a test that we're using c++17
     auto v = std::make_optional(genny::getVersion());
     std::cout << u8"🧞 Genny" << " Version " << v.value_or("ERROR") << u8" 💝🐹🌇⛔" << std::endl;
+    boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::info);
 
     genny::driver::DefaultDriver d;
 
