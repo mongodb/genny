2df5da15cea870399b261a6844a947813519446a
Ryan Timmons <ryan.timmons@10gen.com>
2018-10-23 13:49:52 -0400

    doc it like it's hot

--- src/python/genny/metrics_output_parser.py
+++ src/python/genny/metrics_output_parser.py
@@ -1,9 +1,58 @@
 #!/usr/bin/env python
 
 import json
+"""
+Genny's CSV-ish metrics output looks like the following:
+
+    Clocks
+    SystemTime,1537814143804295
+    MetricsTime,12137713413174
+
+    Counters
+    12137712905436,HelloTest.0.documents,1
+
+    Gauges
+    12137712905436,HelloTest.0.connections,1
+
+    Timers
+    12137712905436,InsertTest.1.output,2235448
+    12137713296139,InsertTest.1.output,278701
+    12137712901949,InsertTest.0.output,2226794
+    12137713293440,InsertTest.0.output,268288
+    12137710737813,HelloTest.1.output,67458
+    12137713066822,HelloTest.1.output,40257
+    12137710753276,HelloTest.0.output,82620
+    12137713053006,HelloTest.0.output,31774
+
+There are 4 "sections": Clocks, Counters, Gauges, and Timers.
+Clocks gives the correspondence between time-points measured
+using the Metrics clock and the System clock. Counters, Gauges,
+and Timers all record values for their respective metrics objects
+(see metrics.hpp in the Genny C++ codebase).
+
+The format for Counters, Gauges, and Timers is:
+
+    Timestamp in metrics-clock-time
+    ,
+    Actor name
+    .
+    Actor thread number (zero-based) (optional)
+    .
+    Actor operation name
+    ,
+    Metric data-point value
+
+For now we only record and operate on the Clocks and Timers
+sections. The other data is only available in the raw CSV,
+not in any rolled-up/summarized form.
+"""
 
 
 class ParserResults(object):
+    """
+    Represents the parsed Genny metrics CSV.
+    """
+
     def __init__(self):
         self.sections = {}
         self.clock_delta = None
@@ -13,23 +62,34 @@ class ParserResults(object):
         # state-tracking
         self.section_lines = []
         self.section_name = None
+        self.done_timers = False
 
     def add_line(self, line):
         """
+        Add a line within the current section. See `start_section()`.
+
         :param list line: already tokenized (split on commas). list of string
         :return: None
         """
         # only care about Clocks and Timers for now
         if self.section_name == 'Clocks':
             self.section_lines.append(line)
-        if self.section_name == 'Timers':
+        elif self.section_name == 'Timers':
+            # metrics csv files are likely to be huge and we don't
+            # need to store the full data-set in-memory, at least
+            # not yet. Instead just stream the line to the event-style
+            # method _on_timer_line().
             self._on_timer_line(line)
 
     def start_section(self, name):
         """
         Indicate that we're done with the current section
-        (the current type of metrics objects being processed)).
+        (the current type of metrics objects being processed)
+        and starts the indicated section.
+
         This automatically calls `.end()`.
+
+        :param name: the section to be started
         """
         self.end()
         self.section_name = name
@@ -42,6 +102,8 @@ class ParserResults(object):
         """
         if self.section_name is not None:
             self.sections[self.section_name] = self.section_lines
+            if self.section_name == 'Timers':
+                self.done_timers = True
         self.section_lines = []
         self.section_name = None
 
@@ -74,6 +136,11 @@ class ParserResults(object):
         if self.clock_delta is not None:
             return metrics_time + self.clock_delta
 
+        if 'Clocks' not in self.sections:
+            msg = "Can only call _system_time after we've seen the Clocks section." +\
+                  "We've seen {}".format(self.sections.keys())
+            raise KeyError(msg)
+
         clocks = {}
         for clock in self.sections['Clocks']:
             name, time = clock[0], int(clock[1])
@@ -83,23 +150,41 @@ class ParserResults(object):
         return self._system_time(metrics_time)
 
     def timers(self):
+        """
+        Can only be called after we've finished the Timers section.
+
+        :return: summarized timer values. Dictionary key is
+                 "Actor Name . operation" and value is a summary (dict) about
+                 all the data-points for that timer.
+        """
         return self._timers
 
+    # TODO: record input line number and report in exceptions
     def _on_timer_line(self, timer_line):
+        """
+        :param timer_line: either [metrics-timestamp, Actor.Thread.Operation, DurationMicroseconds]
+                           or [metrics-timestamp, Actor.Operation, DurationMicroseconds].
+        :return:
+        """
         when = self._system_time(int(timer_line[0]))
         full_event = timer_line[1]
         duration = int(timer_line[2])
 
         event_parts = full_event.split('.')
-        if len(event_parts) < 3:
-            # for Genny.Setup and the like
+        if len(event_parts) == 2:
+            # for Genny.Setup and the like: event_parts is like
+            #    [Genny, Setup]
             event_name = event_parts[0] + '.' + event_parts[1]
             thread = 1
-        else:
+        elif len(event_parts) == 3:
+            # For regular (multi-threaded) cases, event_parts is like
+            #    [MyActor, 1,
             event_name = event_parts[0] + '.' + event_parts[2]
             thread = int(event_parts[1]) + 1  # zero-based indexing
+        else:
+            raise Exception("Invalid event given: [{}]".format(event_parts))
 
-        # first time we've seen data for this metrics-name
+        # first time we've seen data for this timer
         if event_name not in self._timers:
             self._timers[event_name] = {
                 'mean': 0,
@@ -117,22 +202,31 @@ class ParserResults(object):
         event['started'] = min(when, event['started'])
         event['ended'] = max(when, event['ended'])
 
+        # compute as streaming mean
         new_mean = ((event['mean'] * event['n']) + duration) / (event['n'] + 1)
         event['n'] = event['n'] + 1
         event['mean'] = new_mean
 
 
 def parse(path):
+    """
+    :param path: path to metrics csv file
+    :return: the `ParserResults`
+    """
     out = ParserResults()
 
     with open(path, 'r') as f:
         for line in f:
             items = line.strip().split(',')
             if len(items) == 0:
+                # blank line
                 pass
             elif len(items) == 1:
+                # section header
+                # TODO: ensure started section
                 out.start_section(items[0])
             else:
+                # add line to current section
                 out.add_line(items)
 
     out.end()
@@ -141,6 +235,12 @@ def parse(path):
 
 
 def summarize():
+    """
+    Reads metrics CSV from stdin and prints a summary json document to stdout.
+
+    Entry-point (see setup.py).
+    :return: None
+    """
     timers = parse('/dev/stdin').timers()
     out = json.dumps(timers, sort_keys=True, indent=4, separators=(',', ': '))
     print(out)
