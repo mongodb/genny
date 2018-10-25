710a0c348c78756f0dfe46f3e731e11095740c4d
Ryan Timmons <119094+rtimmons@users.noreply.github.com>
2018-10-25 13:48:12 -0400

    TIG-1099: Kill Phasedactor (#34)

--- src/gennylib/include/gennylib/actors/HelloWorld.hpp
+++ src/gennylib/include/gennylib/actors/HelloWorld.hpp
@@ -5,27 +5,27 @@
 
 #include <mongocxx/client_session.hpp>
 
-#include <gennylib/PhasedActor.hpp>
+#include <gennylib/PhaseLoop.hpp>
 
 namespace genny::actor {
 
-class HelloWorld : public genny::PhasedActor {
+class HelloWorld : public genny::Actor {
 
 public:
-    explicit HelloWorld(ActorContext& context, const unsigned int thread);
+    explicit HelloWorld(ActorContext& context, unsigned int thread);
+
+    virtual void run() override;
 
     ~HelloWorld() = default;
 
     static ActorVector producer(ActorContext& context);
 
 private:
-    void doPhase(PhaseNumber phase);
+    struct PhaseConfig;
+    PhaseLoop<PhaseConfig> _loop;
 
     metrics::Timer _outputTimer;
     metrics::Counter _operations;
-    // TODO: for now this is dummy / smoke-test that we have the driver
-    mongocxx::client_session* session;
-    std::string _message;
 };
 
 }  // namespace genny::actor
