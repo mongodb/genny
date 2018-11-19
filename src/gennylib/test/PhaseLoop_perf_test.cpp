ca87688bea2a1511907f8c69ef086f665e2b481f
Ryan Timmons <ryan.timmons@mongodb.com>
2018-11-19 14:43:09 -0500

    more closely-related operations

--- src/gennylib/test/PhaseLoop_perf_test.cpp
+++ src/gennylib/test/PhaseLoop_perf_test.cpp
@@ -3,10 +3,10 @@
 #include <atomic>
 #include <chrono>
 #include <iostream>
-#include <optional>
-#include <vector>
-#include <thread>
 #include <mutex>
+#include <optional>
+#include <thread>
+#include <vector>
 
 #include <yaml-cpp/yaml.h>
 
@@ -48,13 +48,31 @@ struct Increments : public Actor {
     static ActorProducer producer() {
         return [](ActorContext& context) {
             ActorVector out;
-            for(int i=0; i < context.get<int>("Threads"); ++i) {
+            for (int i = 0; i < context.get<int>("Threads"); ++i) {
                 out.push_back(std::make_unique<Increments>(context));
             }
             return out;
         };
     }
 };
+
+struct Runnable {
+    virtual void run() = 0;
+};
+
+struct IncrementsRunnable : public Runnable {
+    static atomic_bool stop;
+
+    void run() override {
+        for (int j = 0; j < 10000; ++j) {
+            if (!stop) {
+                ++increments;
+            }
+        }
+    }
+};
+
+atomic_bool IncrementsRunnable::stop = false;
 
 }  // namespace
 
@@ -72,62 +90,55 @@ TEST_CASE("PhaseLoop performance", "[perf]") {
       - Repeat: 10000
     )");
 
-    WorkloadContext workloadContext {
-        yaml, registry, o, "mongodb://localhost:27017", {Increments::producer()}
-    };
+    WorkloadContext workloadContext{
+        yaml, registry, o, "mongodb://localhost:27017", {Increments::producer()}};
 
     o.addRequiredTokens(500);
 
     std::vector<std::thread> actors;
     boost::barrier actorBarrier(1);
-    for(auto& actor : workloadContext.actors()) {
-        actors.emplace_back([&](){
+    for (auto& actor : workloadContext.actors()) {
+        actors.emplace_back([&]() {
             actorBarrier.wait();
             actor->run();
         });
     }
     auto actorStart = std::chrono::steady_clock::now();
     actorBarrier.count_down_and_wait();
-    for(auto& actor : actors)
+    for (auto& actor : actors)
         actor.join();
-    auto actorDur = duration_cast<milliseconds>(steady_clock::now() - actorStart).count();
-    std::cout << "Took " << actorDur << " milliseconds for PhaseLoop loop" << std::endl;
+    auto actorDur = duration_cast<nanoseconds>(steady_clock::now() - actorStart).count();
+    std::cout << "Took " << actorDur << " nanoseconds for PhaseLoop loop" << std::endl;
 
 
     increments = 0;
 
-    std::atomic_bool stop = false;
-
-    auto fn = [&]() {
-        for(int j=0; j<10000; ++j) {
-            if (!stop) {
-                ++increments;
-            }
-        }
-    };
+    boost::barrier regBarrier(1);
+    std::vector<std::unique_ptr<IncrementsRunnable>> runners;
+    for(int i=0; i<500; ++i) {
+        runners.emplace_back(std::make_unique<IncrementsRunnable>());
+    }
+    std::vector<std::thread> regulars;
+    for (auto& runnable : runners) {
+        regulars.emplace_back([&]() {
+            regBarrier.wait();
+            runnable->run();
+        });
+    }
 
     // keep the compiler from being clever about
     // removing check to `if(!stop)`
     auto stopper = std::thread([&](){
         std::this_thread::sleep_for(milliseconds{5000});
-        stop = true;
+        IncrementsRunnable::stop = true;
     });
-
-    boost::barrier regBarrier(1);
-    std::vector<std::thread> regulars;
-    for(int i=0; i < 500; ++i) {
-        regulars.emplace_back([&](){
-            regBarrier.wait();
-            fn();
-        });
-    }
 
     auto regStart = std::chrono::steady_clock::now();
     regBarrier.count_down_and_wait();
-    for(auto& reg : regulars)
+    for (auto& reg : regulars)
         reg.join();
-    auto regDur = duration_cast<milliseconds>(steady_clock::now() - regStart).count();
-    std::cout << "Took " << regDur << " milliseconds for regular for loop" << std::endl;
+    auto regDur = duration_cast<nanoseconds>(steady_clock::now() - regStart).count();
+    std::cout << "Took " << regDur << " nanoseconds for regular for loop" << std::endl;
 
     stopper.join();
 }
