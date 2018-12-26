99aa80ffea5aa3f5605b854c28fa3000333e5772
Ben Caimano <ben.caimano@10gen.com>
2018-12-26 15:18:53 -0500

    Start leaning on a local cmake modules folder

---
+++ cmake/Options.cmake
@@ -0,0 +1,6 @@
+## Define boolean options and cache variables
+
+option(USE_STATIC_BOOST
+       "Use boost static libaries when compiling genny. \
+        This also requires boost static libraries to have position-independent code."
+       OFF)
