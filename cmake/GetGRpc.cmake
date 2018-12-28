7a019aac3ea5622abf2601e177c508a94756767c
Ben Caimano <ben.caimano@10gen.com>
2018-12-28 17:11:09 -0500

    Incorporated ExternalProject target into gRPC compile script

--- cmake/GetGRpc.cmake
+++ cmake/GetGRpc.cmake
@@ -2,3 +2,85 @@
 ## We use it if it's there, and provide a way to build it if it is not.
 
 find_package(gRPC CONFIG)
+
+# Inspired by https://github.com/grpc/grpc/blob/v1.17.0/examples/cpp/helloworld/cmake_externalproject/CMakeLists.txt
+include(ExternalProject)
+
+## Install c-ares
+set(cares_ARGS
+    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+)
+
+# Only build static libraries
+list(APPEND cares_ARGS
+    -DCARES_SHARED:BOOL=OFF
+    -DCARES_STATIC:BOOL=ON
+    -DCARES_STATIC_PIC:BOOL=ON
+)
+
+ExternalProject_Add(project_cares
+    EXCLUDE_FROM_ALL ON
+
+    GIT_REPOSITORY  git@github.com:c-ares/c-ares.git
+    GIT_TAG         cares-1_13_0
+
+    CMAKE_CACHE_ARGS    ${cares_ARGS}
+)
+
+## Install Protocol Buffers
+set(protobuf_ARGS
+    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+)
+
+# No shared libs and no tests
+list(APPEND protobuf_ARGS
+    -Dprotobuf_BUILD_TESTS:BOOL=OFF
+    -Dprotobuf_BUILD_SHARED_LIBS:BOOL=OFF
+)
+
+ExternalProject_Add(project_protobuf
+    EXCLUDE_FROM_ALL ON
+
+    GIT_REPOSITORY  git@github.com:protocolbuffers/protobuf.git
+    GIT_TAG         v3.6.1
+
+    SOURCE_SUBDIR   "cmake"
+
+    CMAKE_CACHE_ARGS    ${protobuf_ARGS}
+)
+
+## Install gRPC
+set(gRPC_ARGS
+    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_INSTALL_PREFIX}
+    -DgRPC_BUILD_TESTS:BOOL=OFF
+)
+
+# Jump through all the hoops necessary to install gRPC 
+list(APPEND gRPC_ARGS
+    -DgRPC_INSTALL:BOOL=ON
+    -DgRPC_PROTOBUF_PROVIDER:STRING=package
+    -DgRPC_ZLIB_PROVIDER:STRING=package
+    -DgRPC_CARES_PROVIDER:STRING=package
+    -DgRPC_SSL_PROVIDER:STRING=package
+)
+
+# If we have a custom OpenSSL install, use it.
+# (We probably do.)
+if(OPENSSL_ROOT_DIR)
+    list(APPEND gRPC_ARGS
+        -DOPENSSL_ROOT_DIR:PATH=${OPENSSL_ROOT_DIR}
+    )
+endif()
+
+ExternalProject_Add(project_grpc
+    EXCLUDE_FROM_ALL ON
+    DEPENDS
+        project_cares
+        project_protobuf
+
+    GIT_REPOSITORY      git@github.com:grpc/grpc.git
+    GIT_TAG             v1.17.0
+
+    CMAKE_CACHE_ARGS    ${gRPC_ARGS}
+)
--- scripts/make-grpc.sh
+++ scripts/make-grpc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Inspired by https://github.com/grpc/grpc/blob/master/test/distrib/cpp/run_distrib_test_cmake.sh
-
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -10,122 +8,32 @@ set -x
 
 SCRIPTS_DIR="$(dirname "${BASH_SOURCE[0]}")"
 ROOT_DIR="$(cd "${SCRIPTS_DIR}/.." && pwd)"
+BUILD_DIR="${ROOT_DIR}/build/grpc"
 
-GRPC_DIR="${ROOT_DIR}/src/third_party/grpc"
-GRPC_SRC_DIR="${GRPC_DIR}/src"
-GRPC_BUILD_DIR="${GRPC_DIR}/build"
+# Hop into a fresh build dir
+mkdir -p "${BUILD_DIR}"
+rm -rf "${BUILD_DIR}/"*
+cd "${BUILD_DIR}"
 
-cleanGRpc() {
-    rm -rf "${GRPC_DIR}" # Remove grpc and any cmake config copied up from runner
-}
-
-cloneGRpc() {
-    git clone --recurse-submodules https://github.com/grpc/grpc.git "${GRPC_SRC_DIR}"
-
-    cd "${GRPC_SRC_DIR}"
-
-    git checkout b79462f186cc22550bc8d53a00ae751f50d194f5
-}
-
-buildProtobuf() {
-    cd "${GRPC_SRC_DIR}/third_party/protobuf"
-
-    BUILD_DIR="cmake/build"
-    mkdir -p "${BUILD_DIR}"
-    cd "${BUILD_DIR}"
-    rm -rf *
-
-    CMAKE_CMD=(
-        cmake
-        ..
-    )
-
-    CMAKE_CMD+=(
-        -Dprotobuf_BUILD_TESTS=OFF
-        -Dprotobuf_BUILD_SHARED_LIBS=OFF
-    )
-
-    CMAKE_CMD+=(
-        -DCMAKE_INSTALL_PREFIX="${GRPC_SRC_DIR}"
-    )
-
-    "${CMAKE_CMD[@]}"
-    make install
-}
-
-buildCAres(){
-    cd "${GRPC_SRC_DIR}/third_party/cares/cares"
-
-    BUILD_DIR="cmake/build"
-    mkdir -p "${BUILD_DIR}"
-    cd "${BUILD_DIR}"
-    rm -rf *
-
-    CMAKE_CMD=(
-        cmake
-        ../..
-    )
-
-    # Flip c-ares to be static
-    CMAKE_CMD+=(
-        -DCARES_STATIC=ON
-        -DCARES_SHARED=OFF
-        -DCARES_STATIC_PIC=ON
-    )
-
-    CMAKE_CMD+=(
-        -DCMAKE_INSTALL_PREFIX="${GRPC_SRC_DIR}"
-    )
-
-    "${CMAKE_CMD[@]}"
-    make install
-}
-
-buildGRpc() {
-    mkdir -p "${GRPC_BUILD_DIR}"
-    cd "${GRPC_BUILD_DIR}"
-    rm -rf *
-
-    CMAKE_CMD=(
-        cmake
-        "${GRPC_SRC_DIR}"
-    )
-
-    # Install grpc into its own source dir for ease of access
-    CMAKE_CMD+=(
-      -DCMAKE_PREFIX_PATH="${GRPC_DIR}"
-      -DCMAKE_INSTALL_PREFIX="${GRPC_DIR}"
-    )
-
-    # Make gRPC easier to install
-    CMAKE_CMD+=(
-        -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF
-    )
-
-    # Use gRPC build-external instances of these components
-    # zLib is usually system provided
-    # c-ares and protobuf are compiled here
-    # OpenSSL may or may not be compiled here
-    CMAKE_CMD+=(
-        -DgRPC_ZLIB_PROVIDER=package
-        -DgRPC_SSL_PROVIDER=package
-        -DgRPC_PROTOBUF_PROVIDER=package
-        -DgRPC_CARES_PROVIDER=package
-    )
-
-    # Use our custom static OpenSSL if it's there
-    if [[ -n ${OPENSSL_DIR} && -d ${OPENSSL_DIR} ]]; then
-        CMAKE_CMD+=(
-            "-DOPENSSL_ROOT_DIR=${OPENSSL_DIR}"
-        )
+# Find openssl at a set of locations we expect
+OPENSSL_ROOT_DIR=""
+OPENSSL_DIRS=(
+    "/opt/mongodbtoolchain/v2"
+    "/usr/local/opt/openssl@1.1"
+    "/usr/local/opt/openssl"
+    "/usr/local"
+    "/usr"
+)
+for DIR in "${OPENSSL_DIRS[@]}"; do
+    OPENSSL_EXE="${DIR}/bin/openssl"
+    if [[ ! -f $OPENSSL_EXE ]]; then
+        continue
     fi
 
-    "${CMAKE_CMD[@]}"
-    make install
-}
+    OPENSSL_ROOT_DIR="-DOPENSSL_ROOT_DIR:PATH=${DIR}"
+    break
+done
 
-(cleanGRpc)
-(cloneGRpc)
-(buildProtobuf)
-(buildCAres)
-(buildGRpc)
+# Use the mongodbtoolchain openssl to build and install grpc
+cmake "${OPENSSL_ROOT_DIR}" "${ROOT_DIR}" -DCMAKE_PREFIX_PATH="${ROOT_DIR}/../../dist"
+make project_grpc
