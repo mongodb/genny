c6e20738b810da24c1b3d3f76a3484344da45765
Ben Caimano <ben.caimano@10gen.com>
2019-01-07 22:21:32 -0500

    Added push target for genny

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,15 +1,28 @@
 ## Define boolean options and cache variables
 
-option(USE_STATIC_BOOST
-       "Use boost static libaries when compiling genny. \
-        This also requires boost static libraries to have position-independent code."
-       OFF)
+option(ENABLE_SSL
+       "Use SSL for mongo-c-driver and jasperlib."
+       ON)
 
 option(ENABLE_JASPER
        "Enable the jasper framework and include gRPC."
        ON)
 
+if(NOT ENABLE_SSL AND ENABLE_JASPER)
+    message(FATAL "SSL is required for Jasper.")
+endif()
+
 set(GENNY_INSTALL_DIR
     ""
     CACHE PATH "Default install path for genny"
+)
+
+set(GENNY_STATIC_BOOST_PATH
+    ""
+    CACHE PATH "Use a specific static boost install"
+)
+
+option(GENNY_INSTALL_SSL
+       "Install the SSL libraries local to the genny install."
+       OFF
 )
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+set -x
+
+SCRIPTS_DIR="$(dirname "${BASH_SOURCE[0]}")"
+ROOT_DIR="$(cd "${SCRIPTS_DIR}/.." && pwd)"
+BUILD_DIR="${ROOT_DIR}/build"
+
+cd "${BUILD_DIR}"
+rm -rf *
+
+. "${SCRIPTS_DIR}/find-openssl.rc"
+
+"${CMAKE:-cmake}" "${ROOT_DIR}" \
+    "${@:-}"
+
+make
+make install
