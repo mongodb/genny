799dce6881d1a7ae41f9de9dafe5f1051d03daf5
Ryan Timmons <119094+rtimmons@users.noreply.github.com>
2019-01-10 17:10:14 -0500

    TIG-1194: Add more meaningful things to generated Actor (#112)

--- create-new-actor.sh
+++ create-new-actor.sh
@@ -20,7 +20,13 @@ create_header_text() {
     echo "#ifndef $uuid_tag"
     echo "#define $uuid_tag"
     echo ""
+    echo "#include <string_view>"
+    echo ""
+    echo "#include <mongocxx/pool.hpp>"
+    echo ""
     echo "#include <gennylib/Actor.hpp>"
+    echo "#include <gennylib/DefaultRandom.hpp>"
+    echo "#include <gennylib/ExecutionStrategy.hpp>"
     echo "#include <gennylib/PhaseLoop.hpp>"
     echo "#include <gennylib/context.hpp>"
     echo ""
@@ -42,6 +48,10 @@ create_header_text() {
     echo "    void run() override;"
     echo ""
     echo "private:"
+    echo "    ExecutionStrategy _strategy;"
+    echo "    mongocxx::pool::entry _client;"
+    echo "    genny::DefaultRandom _rng;"
+    echo ""
     echo "    struct PhaseConfig;"
     echo "    PhaseLoop<PhaseConfig> _loop;"
     echo "};"
@@ -61,23 +71,61 @@ create_impl_text() {
     echo ""
     echo "#include <memory>"
     echo ""
+    echo "#include <yaml-cpp/yaml.h>"
+    echo ""
+    echo "#include <mongocxx/client.hpp>"
+    echo "#include <mongocxx/collection.hpp>"
+    echo "#include <mongocxx/database.hpp>"
+    echo ""
+    echo "#include <boost/log/trivial.hpp>"
+    echo ""
+    echo "#include <bsoncxx/json.hpp>"
+    echo "#include <gennylib/Cast.hpp>"
+    echo "#include <gennylib/context.hpp>"
+    echo "#include <gennylib/value_generators.hpp>"
+    echo "#include <boost/log/trivial.hpp>"
+    echo ""
+    echo "#include <gennylib/value_generators.hpp>"
+    echo "#include <gennylib/ExecutionStrategy.hpp>"
+    echo ""
+    echo ""
     echo "namespace genny::actor {"
     echo ""
     echo "struct ${actor_name}::PhaseConfig {"
-    echo "    PhaseConfig(PhaseContext& context) {}"
+    echo "    mongocxx::collection collection;"
+    echo "    std::unique_ptr<value_generators::DocumentGenerator> docGen;"
+    echo "    ExecutionStrategy::RunOptions options;"
+    echo ""
+    echo "    PhaseConfig(PhaseContext& phaseContext, genny::DefaultRandom& rng, const mongocxx::database& db)"
+    echo "        : collection{db[phaseContext.get<std::string>(\"Collection\")]},"
+    echo "          docGen{value_generators::makeDoc(phaseContext.get(\"Document\"), rng)},"
+    echo "          options{ExecutionStrategy::getOptionsFrom(phaseContext, \"ExecutionsStrategy\")} {}"
     echo "};"
     echo ""
     echo "void ${actor_name}::run() {"
     echo "    for (auto&& config : _loop) {"
-    echo "        for (auto&& _ : config) {"
-    echo "            // TODO: main logic"
+    echo "        for (const auto&& _ : config) {"
+    echo "            _strategy.run("
+    echo "                [&]() {"
+    echo "                    // TODO: main logic"
+    echo "                    bsoncxx::builder::stream::document mydoc{};"
+    echo "                    auto view = config->docGen->view(mydoc);"
+    echo "                    BOOST_LOG_TRIVIAL(info) << \" ${actor_name} Inserting \" << bsoncxx::to_json(view);"
+    echo "                    config->collection.insert_one(view);"
+    echo "                },"
+    echo "                config->options);"
     echo "        }"
+    echo ""
+    echo "        _strategy.recordMetrics();"
     echo "    }"
     echo "}"
     echo ""
-    echo "${actor_name}::${actor_name}(ActorContext& context)"
+    echo "${actor_name}::${actor_name}(genny::ActorContext& context)"
     echo "    : Actor(context),"
-    echo "      _loop{context} {}"
+    echo "      _rng{context.workload().createRNG()},"
+    echo "      _strategy{context, ${actor_name}::id(), \"insert\"},"
+    echo "      _client{std::move(context.client())},"
+    echo "      _loop{context, _rng, (*_client)[context.get<std::string>(\"Database\")]} {}"
     echo ""
     echo "namespace {"
     echo "auto register${actor_name} = Cast::registerDefault<${actor_name}>();"
@@ -107,39 +155,64 @@ create_test() {
     local actor_name
     actor_name="$1"
 
-    cat << EOF >> "$(dirname "$0")/src/gennylib/test/${actor_name}_test.cpp"
+    cat << EOF > "$(dirname "$0")/src/gennylib/test/${actor_name}_test.cpp"
 #include "test.h"
 
 #include <bsoncxx/json.hpp>
 #include <bsoncxx/builder/stream/document.hpp>
 
+#include <boost/exception/diagnostic_information.hpp>
+
+#include <yaml-cpp/yaml.h>
+
 #include <MongoTestFixture.hpp>
+#include <ActorHelper.hpp>
+
+#include <gennylib/context.hpp>
 
 namespace {
 using namespace genny::testing;
 namespace bson_stream = bsoncxx::builder::stream;
 
 TEST_CASE_METHOD(MongoTestFixture, "${actor_name} successfully connects to a MongoDB instance.",
-          "[standalone][single_node_replset][three_node_replset][sharded]") {
+          "[standalone][single_node_replset][three_node_replset][sharded][${actor_name}]") {
 
     dropAllDatabases();
-    auto db = client.database("test");
+    auto db = client.database("mydb");
 
-    SECTION("Insert a document into the database.") {
-        auto builder = bson_stream::document{};
-        bsoncxx::document::value doc_value = builder
-                << "name" << "MongoDB"
-                << "type" << "database"
-                << "count" << 1
-                << "info" << bson_stream::open_document
-                << "x" << 203
-                << "y" << 102
-                << bson_stream::close_document
-                << bson_stream::finalize;
-        bsoncxx::document::view view = doc_value.view();
-        db.collection("test").insert_one(view);
-        // Fail on purpose to encourage contributors to extend automated testing for each new actor.
-        REQUIRE(db.collection("test").count(view) == 0);
+    YAML::Node config = YAML::Load(R"(
+        SchemaVersion: 2018-07-01
+        Actors:
+        - Name: ${actor_name}
+          Type: ${actor_name}
+          Database: mydb
+          ExecutionStrategy:
+            ThrowOnFailure: true
+          Phases:
+          - Repeat: 100
+            Collection: mycoll
+            Document: {foo: {\$randomint: {min: 0, max: 100}}}
+    )");
+
+
+    SECTION("Inserts documents into the database.") {
+        try {
+            genny::ActorHelper ah(config, 1, MongoTestFixture::kConnectionString.to_string());
+            ah.run([](const genny::WorkloadContext& wc) { wc.actors()[0]->run(); });
+
+            auto builder = bson_stream::document{};
+            builder << "foo" << bson_stream::open_document
+                    << "\$gte" << "0" << bson_stream::close_document
+                    << bson_stream::finalize;
+
+            auto count = db.collection("mycoll").count_documents(builder.view());
+            // TODO: fixme
+            REQUIRE(count == 101);
+        } catch (const std::exception& e) {
+            auto diagInfo = boost::diagnostic_information(e);
+            INFO("CAUGHT " << diagInfo);
+            FAIL(diagInfo);
+        }
     }
 }
 } // namespace
@@ -198,3 +271,12 @@ create_impl                  "$uuid_tag" "$actor_name"
 recreate_cast_core_cmake_file "$uuid_tag" "$actor_name"
 create_test                  "$actor_name"
 recreate_gennylib_cmake_file "$uuid_tag" "$actor_name"
+
+echo "Successfully generated skeleton for ${actor_name}. Build and test ${actor_name} with the following command:"
+echo ""
+echo "    cd build"
+echo "    cmake .."
+echo "    make -j8"
+echo "    ./src/gennylib/test_gennylib_with_server '[${actor_name}]'"
+echo "    make test"
+echo ""
