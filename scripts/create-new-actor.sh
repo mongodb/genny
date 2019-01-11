8525f3b5c5da67b1b79de2c5fb4878edb94951bc
Ryan Timmons <ryan.timmons@10gen.com>
2019-01-11 13:27:51 -0500

    add license to generated files

--- scripts/create-new-actor.sh
+++ scripts/create-new-actor.sh
@@ -2,6 +2,8 @@
 
 set -eou pipefail
 
+year="$(date '+%Y')"
+
 usage() {
     echo "Usage:"
     echo ""
@@ -17,6 +19,20 @@ create_header_text() {
     uuid_tag="$1"
     actor_name="$2"
 
+    echo "// Copyright ${year} MongoDB Inc."
+    echo "//"
+    echo "// Licensed under the Apache License, Version 2.0 (the \"License\");"
+    echo "// you may not use this file except in compliance with the License."
+    echo "// You may obtain a copy of the License at"
+    echo "//"
+    echo "// http://www.apache.org/licenses/LICENSE-2.0"
+    echo "//"
+    echo "// Unless required by applicable law or agreed to in writing, software"
+    echo "// distributed under the License is distributed on an \"AS IS\" BASIS,"
+    echo "// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."
+    echo "// See the License for the specific language governing permissions and"
+    echo "// limitations under the License."
+    echo ""
     echo "#ifndef $uuid_tag"
     echo "#define $uuid_tag"
     echo ""
@@ -67,6 +83,20 @@ create_impl_text() {
     uuid_tag="$1"
     actor_name="$2"
 
+    echo "// Copyright ${year} MongoDB Inc."
+    echo "//"
+    echo "// Licensed under the Apache License, Version 2.0 (the \"License\");"
+    echo "// you may not use this file except in compliance with the License."
+    echo "// You may obtain a copy of the License at"
+    echo "//"
+    echo "// http://www.apache.org/licenses/LICENSE-2.0"
+    echo "//"
+    echo "// Unless required by applicable law or agreed to in writing, software"
+    echo "// distributed under the License is distributed on an \"AS IS\" BASIS,"
+    echo "// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."
+    echo "// See the License for the specific language governing permissions and"
+    echo "// limitations under the License."
+    echo ""
     echo "#include <cast_core/actors/${actor_name}.hpp>"
     echo ""
     echo "#include <memory>"
@@ -180,6 +210,20 @@ create_test() {
     actor_name="$1"
 
     cat << EOF > "$(dirname "$0")/../src/gennylib/test/${actor_name}_test.cpp"
+// Copyright ${year} MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "test.h"
 
 #include <bsoncxx/json.hpp>
