2aaae0cf38bbb8db727dc72a4d0015227d8128ad
Ryan Timmons <ryan.timmons@10gen.com>
2019-01-11 13:28:46 -0500

    Revert "Add license header"
    This reverts commit 3b00a72b491bdec6fe7015a733db6c2a19a28d25.

--- src/gennylib/include/gennylib/PhaseLoop.hpp
+++ src/gennylib/include/gennylib/PhaseLoop.hpp
@@ -1,17 +1,3 @@
-// Copyright 2018 MongoDB Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #ifndef HEADER_10276107_F885_4F2C_B99B_014AF3B4504A_INCLUDED
 #define HEADER_10276107_F885_4F2C_B99B_014AF3B4504A_INCLUDED
 
