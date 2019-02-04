844d4f670bba2ff9e65821b562981ef94277a609
Robert Guo <robert.guo@10gen.com>
2019-02-04 14:18:41 -0500

    use static mongodb driver

--- cmake/FindMongoCxx.cmake
+++ cmake/FindMongoCxx.cmake
@@ -1,25 +1,31 @@
-find_package(libmongocxx REQUIRED)
+find_package(libmongocxx-static REQUIRED)
+find_package(libmongoc-static-1.0 CONFIG REQUIRED)
 
 # Make a target for bsoncxx
-add_library(MongoCxx::bsoncxx SHARED IMPORTED GLOBAL)
+add_library(MongoCxx::bsoncxx STATIC IMPORTED GLOBAL)
 set_target_properties(MongoCxx::bsoncxx
     PROPERTIES
-        IMPORTED_LOCATION   "${LIBBSONCXX_LIBRARY_PATH}"
+        IMPORTED_LOCATION   "${LIBBSONCXX_STATIC_LIBRARY_PATH}"
 )
 target_include_directories(MongoCxx::bsoncxx
     INTERFACE
-        ${LIBBSONCXX_INCLUDE_DIRS}
+        ${LIBMONGOCXX_STATIC_INCLUDE_DIRS}
+)
+
+target_link_libraries(MongoCxx::bsoncxx
+    INTERFACE
+        ${MONGOC_STATIC_LIBRARIES}
 )
 
 # Make a target for mongocxx
-add_library(MongoCxx::mongocxx SHARED IMPORTED GLOBAL)
+add_library(MongoCxx::mongocxx STATIC IMPORTED GLOBAL)
 set_target_properties(MongoCxx::mongocxx
     PROPERTIES
-        IMPORTED_LOCATION   "${LIBMONGOCXX_LIBRARY_PATH}"
+        IMPORTED_LOCATION   "${LIBMONGOCXX_STATIC_LIBRARY_PATH}"
 )
 target_include_directories(MongoCxx::mongocxx
     INTERFACE
-        ${LIBMONGOCXX_INCLUDE_DIRS}
+        ${LIBMONGOCXX_STATIC_INCLUDE_DIRS}
 )
 target_link_libraries(MongoCxx::mongocxx
     INTERFACE
