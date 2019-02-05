62ddaf2a336fc3c449c346b68ec516f1a52d7a46
Robert Guo <robert.guo@10gen.com>
2019-02-04 20:10:58 -0500

    Revert "use static mongodb driver"
    This reverts commit 844d4f670bba2ff9e65821b562981ef94277a609.

--- cmake/FindMongoCxx.cmake
+++ cmake/FindMongoCxx.cmake
@@ -1,31 +1,25 @@
-find_package(libmongocxx-static REQUIRED)
-find_package(libmongoc-static-1.0 CONFIG REQUIRED)
+find_package(libmongocxx REQUIRED)
 
 # Make a target for bsoncxx
-add_library(MongoCxx::bsoncxx STATIC IMPORTED GLOBAL)
+add_library(MongoCxx::bsoncxx SHARED IMPORTED GLOBAL)
 set_target_properties(MongoCxx::bsoncxx
     PROPERTIES
-        IMPORTED_LOCATION   "${LIBBSONCXX_STATIC_LIBRARY_PATH}"
+        IMPORTED_LOCATION   "${LIBBSONCXX_LIBRARY_PATH}"
 )
 target_include_directories(MongoCxx::bsoncxx
     INTERFACE
-        ${LIBMONGOCXX_STATIC_INCLUDE_DIRS}
-)
-
-target_link_libraries(MongoCxx::bsoncxx
-    INTERFACE
-        ${MONGOC_STATIC_LIBRARIES}
+        ${LIBBSONCXX_INCLUDE_DIRS}
 )
 
 # Make a target for mongocxx
-add_library(MongoCxx::mongocxx STATIC IMPORTED GLOBAL)
+add_library(MongoCxx::mongocxx SHARED IMPORTED GLOBAL)
 set_target_properties(MongoCxx::mongocxx
     PROPERTIES
-        IMPORTED_LOCATION   "${LIBMONGOCXX_STATIC_LIBRARY_PATH}"
+        IMPORTED_LOCATION   "${LIBMONGOCXX_LIBRARY_PATH}"
 )
 target_include_directories(MongoCxx::mongocxx
     INTERFACE
-        ${LIBMONGOCXX_STATIC_INCLUDE_DIRS}
+        ${LIBMONGOCXX_INCLUDE_DIRS}
 )
 target_link_libraries(MongoCxx::mongocxx
     INTERFACE
