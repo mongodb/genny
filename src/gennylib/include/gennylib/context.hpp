982cbf81c9612a62435afd55571b6d17556ec873
Max Hirschhorn <max.hirschhorn@mongodb.com>
2019-02-23 11:38:52 -0500

    Revert "Change nextActorId() to start at 1."
    This reverts commit 18dca89ebcc36669e19d3db0f9eddb447726c453.

--- src/gennylib/include/gennylib/context.hpp
+++ src/gennylib/include/gennylib/context.hpp
@@ -133,9 +133,7 @@ public:
      * @return The next sequential id
      */
     ActorId nextActorId() {
-        // We treat an ActorId of 0 in metrics reporting as though an ActorId wasn't specified. We
-        // therefore have nextActorId() start at 1 to distinguish between these two cases.
-        return ++_nextActorId;
+        return _nextActorId++;
     }
 
     /**
