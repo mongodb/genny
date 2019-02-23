565f80cca2b884516ad0d7e03cac48eac6e16900
Max Hirschhorn <max.hirschhorn@mongodb.com>
2019-02-23 14:11:30 -0500

    Update test case to have operations be concurrent.

--- src/metrics/test/metrics_test.cpp
+++ src/metrics/test/metrics_test.cpp
@@ -152,100 +152,90 @@ TEST_CASE("metrics output format") {
     auto metrics = v1::RegistryT<RegistryClockSourceStub>{};
     auto reporter = genny::metrics::v1::ReporterT{metrics};
 
-    // TODO: Consider changing this test so the operations between actor threads are reported
-    // "concurrently" with respect to how op.start() and RegistryClockSourceStub::advance() are
-    // called.
-    //
-    //           +---------------------+--------------+
-    // Thread 1: |        Insert       |    Remove    |
-    //           +----+                +-+        +---+
-    // Thread 2:      |      Insert      | Remove |
-    //                +--+           +---+--------+
+    //           +---------------------+----------------+
+    // Thread 1: |        Insert       |     Remove     |
+    //           +---------------------+----------------+
+    //                +------------------+  +--------+
+    // Thread 2:      |      Insert      |  | Remove |
+    //                +------------------+  +--------+
+    //                   +-----------+
     // Thread 3:         | Greetings |
     //                   +-----------+
 
-    RegistryClockSourceStub::advance(5ns);
-    {
-        auto op = metrics.operation(1u, "InsertRemove", "Remove");
-        auto ctx = op.start();
-        ctx.addDocuments(6);
-        ctx.addBytes(40);
+    auto insert1 = metrics.operation(1u, "InsertRemove", "Insert");
+    auto insert2 = metrics.operation(2u, "InsertRemove", "Insert");
+    auto remove1 = metrics.operation(1u, "InsertRemove", "Remove");
+    auto remove2 = metrics.operation(2u, "InsertRemove", "Remove");
+    auto greetings3 = metrics.operation(3u, "HelloWorld", "Greetings");
 
-        RegistryClockSourceStub::advance(5ns);
-        ctx.success();
-    }
     RegistryClockSourceStub::advance(5ns);
-    {
-        auto op = metrics.operation(2u, "InsertRemove", "Remove");
-        auto ctx = op.start();
-        ctx.addDocuments(7);
-        ctx.addBytes(30);
+    auto insert1Ctx = insert1.start();
 
-        RegistryClockSourceStub::advance(10ns);
-        ctx.success();
-    }
     RegistryClockSourceStub::advance(5ns);
-    {
-        auto op = metrics.operation(3u, "HelloWorld", "Greetings");
-        auto ctx = op.start();
-        ctx.addIterations(2);
+    auto insert2Ctx = insert2.start();
 
-        RegistryClockSourceStub::advance(15ns);
-        ctx.success();
-    }
-    RegistryClockSourceStub::advance(5ns);
-    {
-        auto op = metrics.operation(2u, "InsertRemove", "Insert");
-        auto ctx = op.start();
-        ctx.addDocuments(8);
-        ctx.addBytes(200);
+    RegistryClockSourceStub::advance(3ns);
+    auto greetings3Ctx = greetings3.start();
 
-        RegistryClockSourceStub::advance(15ns);
-        ctx.success();
-    }
-    RegistryClockSourceStub::advance(5ns);
-    {
-        auto op = metrics.operation(1u, "InsertRemove", "Insert");
-        auto ctx = op.start();
-        ctx.addDocuments(9);
-        ctx.addBytes(300);
+    RegistryClockSourceStub::advance(13ns);
+    greetings3Ctx.addIterations(2);
+    greetings3Ctx.success();
 
-        RegistryClockSourceStub::advance(20ns);
-        ctx.success();
-    }
-    RegistryClockSourceStub::advance(5ns);
+    RegistryClockSourceStub::advance(2ns);
+    insert1Ctx.addDocuments(9);
+    insert1Ctx.addBytes(300);
+    insert1Ctx.success();
+    auto remove1Ctx = remove1.start();
+
+    RegistryClockSourceStub::advance(2ns);
+    insert2Ctx.addDocuments(8);
+    insert2Ctx.addBytes(200);
+    insert2Ctx.success();
+
+    RegistryClockSourceStub::advance(2ns);
+    auto remove2Ctx = remove2.start();
+
+    RegistryClockSourceStub::advance(10ns);
+    remove2Ctx.addDocuments(7);
+    remove2Ctx.addBytes(30);
+    remove2Ctx.success();
+
+    RegistryClockSourceStub::advance(3ns);
+    remove1Ctx.addDocuments(6);
+    remove1Ctx.addBytes(40);
+    remove1Ctx.success();
 
     SECTION("csv reporting") {
         auto expected =
             "Clocks\n"
             "SystemTime,42000000\n"
-            "MetricsTime,95\n"
+            "MetricsTime,45\n"
             "\n"
             "Counters\n"
-            "45,HelloWorld.id-3.Greetings_bytes,0\n"
-            "90,InsertRemove.id-1.Insert_bytes,300\n"
-            "65,InsertRemove.id-2.Insert_bytes,200\n"
-            "25,InsertRemove.id-2.Remove_bytes,30\n"
-            "10,InsertRemove.id-1.Remove_bytes,40\n"
-            "45,HelloWorld.id-3.Greetings_docs,0\n"
-            "90,InsertRemove.id-1.Insert_docs,9\n"
-            "65,InsertRemove.id-2.Insert_docs,8\n"
-            "25,InsertRemove.id-2.Remove_docs,7\n"
-            "10,InsertRemove.id-1.Remove_docs,6\n"
-            "45,HelloWorld.id-3.Greetings_iters,2\n"
-            "90,InsertRemove.id-1.Insert_iters,1\n"
-            "65,InsertRemove.id-2.Insert_iters,1\n"
-            "25,InsertRemove.id-2.Remove_iters,1\n"
-            "10,InsertRemove.id-1.Remove_iters,1\n"
+            "26,HelloWorld.id-3.Greetings_bytes,0\n"
+            "42,InsertRemove.id-2.Remove_bytes,30\n"
+            "45,InsertRemove.id-1.Remove_bytes,40\n"
+            "30,InsertRemove.id-2.Insert_bytes,200\n"
+            "28,InsertRemove.id-1.Insert_bytes,300\n"
+            "26,HelloWorld.id-3.Greetings_docs,0\n"
+            "42,InsertRemove.id-2.Remove_docs,7\n"
+            "45,InsertRemove.id-1.Remove_docs,6\n"
+            "30,InsertRemove.id-2.Insert_docs,8\n"
+            "28,InsertRemove.id-1.Insert_docs,9\n"
+            "26,HelloWorld.id-3.Greetings_iters,2\n"
+            "42,InsertRemove.id-2.Remove_iters,1\n"
+            "45,InsertRemove.id-1.Remove_iters,1\n"
+            "30,InsertRemove.id-2.Insert_iters,1\n"
+            "28,InsertRemove.id-1.Insert_iters,1\n"
             "\n"
             "Gauges\n"
             "\n"
             "Timers\n"
-            "45,HelloWorld.id-3.Greetings_timer,15\n"
-            "90,InsertRemove.id-1.Insert_timer,20\n"
-            "65,InsertRemove.id-2.Insert_timer,15\n"
-            "25,InsertRemove.id-2.Remove_timer,10\n"
-            "10,InsertRemove.id-1.Remove_timer,5\n"
+            "26,HelloWorld.id-3.Greetings_timer,13\n"
+            "42,InsertRemove.id-2.Remove_timer,10\n"
+            "45,InsertRemove.id-1.Remove_timer,17\n"
+            "30,InsertRemove.id-2.Insert_timer,20\n"
+            "28,InsertRemove.id-1.Insert_timer,23\n"
             "\n";
 
         std::ostringstream out;
