abafc40153746c2ecd81dd4e73f0ba44e6a66423
Robert Guo <robert.guo@10gen.com>
2019-02-25 14:15:33 -0500

    allow column order to change

--- src/python/genny/csv2.py
+++ src/python/genny/csv2.py
@@ -29,6 +29,31 @@ class CSV2ParsingError(BaseException):
     pass
 
 
+class CSVColumns(object):
+    """
+    Object oriented access to csv header/column mapping.
+    """
+    _COLUMNS = set()
+
+    @classmethod
+    def add_columns(cls, columns):
+        for i in range(len(columns)):
+            cls.add_column(columns[i], i)
+
+    @classmethod
+    def add_column(cls, col_name, col_index):
+        upper_col_name = col_name.upper()  # Python class constants should be uppercase.
+        if not hasattr(cls, upper_col_name):
+            raise CSV2ParsingError('%s doesn\'t have column %s', cls.__name__, col_name)
+        setattr(cls, upper_col_name, col_index)
+        cls._COLUMNS.add(upper_col_name)
+
+    @classmethod
+    def validate(cls, expected_cols_set):
+        val = cls._COLUMNS == expected_cols_set
+        return val
+
+
 class _DataReader:
     """
     Thin wrapper around csv.DictReader() that eagerly converts any digits to native
@@ -57,18 +82,18 @@ class _DataReader:
                 line[i] = int(line[i])
 
         # Eagerly error if OUTCOME is > 1
-        if line[_Columns.OUTCOME] > 1:
+        if line[_OpColumns.OUTCOME] > 1:
             raise CSV2ParsingError('Unexpected outcome on line %d: %s', self.raw_reader.line_num,
                                    line)
 
         # Convert nanoseconds to milliseconds and add offset
-        line[_Columns.TIMESTAMP] /= 1000 * 1000
-        line[_Columns.TIMESTAMP] += self.ts_offset
+        line[_OpColumns.TIMESTAMP] /= 1000 * 1000
+        line[_OpColumns.TIMESTAMP] += self.ts_offset
 
         # Remove the actor and operation columns to save space.
-        assert _Columns.ACTOR < _Columns.OPERATION
-        op = line.pop(_Columns.OPERATION)
-        actor = line.pop(_Columns.ACTOR)
+        assert _OpColumns.ACTOR < _OpColumns.OPERATION
+        op = line.pop(_OpColumns.OPERATION)
+        actor = line.pop(_OpColumns.ACTOR)
 
         # Append the thread count to the last column
         line.append(self.tc_map[(actor, op)])
@@ -76,17 +101,35 @@ class _DataReader:
         return line, actor, op
 
 
-class _Columns:
-    TIMESTAMP = 0
-    ACTOR = 1
-    THREAD = 2
-    OPERATION = 3
-    DURATION = 4
-    OUTCOME = 5
-    N = 6
-    OPS = 7
-    ERRORS = 8
-    SIZE = 9
+class _OpColumns(CSVColumns):
+    _COLUMNS = set()
+
+    TIMESTAMP = None
+    ACTOR = None
+    THREAD = None
+    OPERATION = None
+    DURATION = None
+    OUTCOME = None
+    N = None
+    OPS = None
+    ERRORS = None
+    SIZE = None
+
+
+class _ClockColumns(CSVColumns):
+    _COLUMNS = set()
+
+    CLOCK = None
+    NANOSECONDS = None
+
+
+# OperationThreadCount columns.
+class _TCColumns(CSVColumns):
+    _COLUMNS = set()
+
+    ACTOR = None
+    OPERATION = None
+    WORKERS = None
 
 
 class CSV2:
@@ -144,9 +187,10 @@ class CSV2:
 
     def _parse_clocks(self, reader):
         title = next(reader)[0]
-        headers = [h.strip() for h in next(reader)]
-        if title != 'Clocks' or headers != ['clock', 'nanoseconds']:
-            raise CSV2ParsingError('Unexpected values in lines %s, %s', title, headers)
+        raw_headers = next(reader)
+        _ClockColumns.add_columns([header.strip() for header in raw_headers])
+        if title != 'Clocks' or not _ClockColumns.validate({'CLOCK', 'NANOSECONDS'}):
+            raise CSV2ParsingError('Unexpected values in lines %s, %s', title, raw_headers)
 
         unix_time_line = next(reader)
         metrics_time_line = next(reader)
@@ -163,9 +207,11 @@ class CSV2:
 
     def _parse_thread_count(self, reader):
         title = next(reader)[0]
-        headers = [h.strip() for h in next(reader)]
-        if title != 'OperationThreadCounts' or headers != ['actor', 'operation', 'workers']:
-            raise CSV2ParsingError('Unexpected values in lines %s, %s', title, headers)
+        raw_headers = next(reader)
+        _TCColumns.add_columns([h.strip() for h in raw_headers])
+        if title != 'OperationThreadCounts' or not _TCColumns.validate(
+                {'ACTOR', 'OPERATION', 'WORKERS'}):
+            raise CSV2ParsingError('Unexpected values in lines %s, %s', title, raw_headers)
 
         for line in reader:
             if not line:
@@ -179,8 +225,9 @@ class CSV2:
 
     def _parse_operations(self, reader):
         title = next(reader)[0]
-        headers = [h.strip() for h in next(reader)]
-        expected_headers = ['timestamp', 'actor', 'thread', 'operation', 'duration', 'outcome', 'n',
-                            'ops', 'errors', 'size']
-        if title != 'Operations' or headers != expected_headers:
-            raise CSV2ParsingError('Unexpected values in lines %s, %s', title, headers)
+        raw_headers = next(reader)
+        _OpColumns.add_columns([h.strip() for h in raw_headers])
+        expected_headers = {'TIMESTAMP', 'ACTOR', 'THREAD', 'OPERATION', 'DURATION', 'OUTCOME', 'N',
+                            'OPS', 'ERRORS', 'SIZE'}
+        if title != 'Operations' or not _OpColumns.validate(expected_headers):
+            raise CSV2ParsingError('Unexpected values in lines %s, %s', title, raw_headers)
