a7f6b9dbf77ad6f9ec55e89d081d9a3e9edbef8b
Robert Guo <robert.guo@10gen.com>
2019-02-28 09:12:34 -0500

    fix constexpr

--- src/gennylib/include/gennylib/v1/Sleeper.hpp
+++ src/gennylib/include/gennylib/v1/Sleeper.hpp
@@ -28,13 +28,13 @@ class Sleeper {
 public:
     Sleeper(Duration before, Duration after) : _before(before), _after(after) {}
 
-    void before(const Orchestrator& o, const PhaseNumber pn) {
+    constexpr void before(const Orchestrator& o, const PhaseNumber pn) {
         if (_before.count() && o.currentPhase() == pn) {
             std::this_thread::sleep_for(_before);
         }
     }
 
-    void after(const Orchestrator& o, const PhaseNumber pn) {
+    constexpr void after(const Orchestrator& o, const PhaseNumber pn) {
         if (_after.count() && o.currentPhase() == pn) {
             std::this_thread::sleep_for(_after);
         }
