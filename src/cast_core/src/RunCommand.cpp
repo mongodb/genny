65c0aabc9ea1d047cf7f08638fb130ef447c521c
Robert Guo <robert.guo@10gen.com>
2019-03-25 17:52:32 -0400

    remove unrelated change

--- src/cast_core/src/RunCommand.cpp
+++ src/cast_core/src/RunCommand.cpp
@@ -36,7 +36,6 @@
 #include <value_generators/DocumentGenerator.hpp>
 
 namespace {
-using namespace bsoncxx::builder;
 /**
  * @private
  * @param exception exception received from mongocxx::database::run_command()
