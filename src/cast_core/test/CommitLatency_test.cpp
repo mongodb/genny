fb210f0b7e6a8c89cb3ba7629bd8366952e1884b
Henrik Ingo <henrik.ingo@mongodb.com>
2019-03-25 20:26:01 +0200

    Write a real test into the unit test template

--- src/cast_core/test/CommitLatency_test.cpp
+++ src/cast_core/test/CommitLatency_test.cpp
@@ -27,42 +27,55 @@
 
 namespace {
 using namespace genny::testing;
-namespace bson_stream = bsoncxx::builder::stream;
 
-TEST_CASE_METHOD(MongoTestFixture, "CommitLatency successfully connects to a MongoDB instance.",
-          "[standalone][single_node_replset][three_node_replset][sharded][CommitLatency]") {
+TEST_CASE_METHOD(MongoTestFixture, "CommitLatency",
+          "[single_node_replset][three_node_replset][sharded][CommitLatency]") {
 
     dropAllDatabases();
     auto db = client.database("mydb");
 
     YAML::Node config = YAML::Load(R"(
         SchemaVersion: 2018-07-01
+        Database: mydb
+        Collection: &Collection CommitLatency
         Actors:
         - Name: CommitLatency
           Type: CommitLatency
-          Database: mydb
-          ExecutionStrategy:
-            ThrowOnFailure: true
+          Threads: 1
+          Repeat: 500
+          Database: test
           Phases:
-          - Repeat: 100
-            Collection: mycoll
-            Document: {foo: {^RandomInt: {min: 0, max: 100}}}
+           - WriteConcern: 0
+             ReadConcern: local
+             ReadPreference: PRIMARY
+           - WriteConcernMajority: true
+             ReadConcern: snapshot
+             ReadPreference: PRIMARY
+             Transaction: True         # Implies Session
+
     )");
 
 
-    SECTION("Inserts documents into the database.") {
+    SECTION("With and without transactions.") {
         try {
+            // First insert 2 documents: [{_id: 1, n: 100}, {_id: 2, n: 100}]
+            auto coll = db["CommitLatency"];
+            auto builder = bsoncxx::builder::stream::document{};
+            builder << "_id" << 1
+                    << "n" << 100
+                    << bsoncxx::builder::stream::finalize;
+            coll.insert_one(builder.view());
+            auto builder2 = bsoncxx::builder::stream::document{};
+            builder2 << "_id" << 1
+                    << "n" << 100
+                    << bsoncxx::builder::stream::finalize;
+            coll.insert_one(builder2.view());
+
             genny::ActorHelper ah(config, 1, MongoTestFixture::connectionUri().to_string());
             ah.run([](const genny::WorkloadContext& wc) { wc.actors()[0]->run(); });
 
-            auto builder = bson_stream::document{};
-            builder << "foo" << bson_stream::open_document
-                    << "$gte" << "0" << bson_stream::close_document
-                    << bson_stream::finalize;
-
-            auto count = db.collection("mycoll").count_documents(builder.view());
-            // TODO: fixme
-            REQUIRE(count == 101);
+            auto count = db.collection("CommitLatency").estimated_document_count();
+            REQUIRE(count == 2);
         } catch (const std::exception& e) {
             auto diagInfo = boost::diagnostic_information(e);
             INFO("CAUGHT " << diagInfo);
