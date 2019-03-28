7e8fa4f5151131014e8f6249cf25341591378583
Robert Guo <robert.guo@10gen.com>
2019-03-28 17:57:23 -0400

    fix help

--- src/driver/src/main.cpp
+++ src/driver/src/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
     auto opts = genny::driver::DefaultDriver::ProgramOptions(argc, argv);
     if (opts.isHelp) {
 
-        std::cout << u8"🧞 Genny Usage: " << argv[0] << " <subcommand> [options] <workload-file> \n";
+        std::cout << u8"🧞 Genny Usage: " << argv[0] << " <subcommand> [options] <workload-file [workload-file]> \n";
         std::cout << opts.description << std::endl;
         return 0;
     }
