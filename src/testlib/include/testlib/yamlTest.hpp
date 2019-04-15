5836994d54825d7dddbd070ce4eef344a737458c
Ryan Timmons <119094+rtimmons@users.noreply.github.com>
2019-04-15 16:57:47 -0400

    TIG-1397: Move some YAML-Based Testing Infrastructure to testlib (#216)

---
+++ src/testlib/include/testlib/yamlTest.hpp
@@ -0,0 +1,82 @@
+// Copyright 2019-present MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef HEADER_8A80B79E_1A24_4745_BB59_B1FCD1439C6D_INCLUDED
+#define HEADER_8A80B79E_1A24_4745_BB59_B1FCD1439C6D_INCLUDED
+
+#include <string>
+#include <vector>
+
+#include <yaml-cpp/yaml.h>
+
+namespace genny::testing::v1 {
+
+template <typename TC>
+class YamlTests {
+public:
+    explicit YamlTests() = default;
+    YamlTests(const YamlTests&) = default;
+
+    explicit YamlTests(const YAML::Node& node) {
+        for (auto&& n : node["Tests"]) {
+            _cases.push_back(std::move(n.as<TC>()));
+        }
+    }
+
+    void run() const {
+        for (auto& tcase : _cases) {
+            tcase.run();
+        }
+    }
+
+private:
+    std::vector<TC> _cases;
+};
+
+}  // namespace genny::testing::v1
+
+namespace YAML {
+
+template <typename TC>
+struct convert<genny::testing::v1::YamlTests<TC>> {
+    static Node encode(const genny::testing::v1::YamlTests<TC>& rhs) {
+        return {};
+    }
+    static bool decode(const Node& node, genny::testing::v1::YamlTests<TC>& rhs) {
+        rhs = genny::testing::v1::YamlTests<TC>(node);
+        return true;
+    }
+};
+
+}  // namespace YAML
+
+namespace genny::testing {
+
+template <typename TC>
+void runTestCaseYaml(const std::string& repoRelativePathToYaml) {
+    try {
+        const auto file = genny::findRepoRoot() + repoRelativePathToYaml;
+        const auto yaml = ::YAML::LoadFile(file);
+        auto tests = yaml.as<genny::testing::v1::YamlTests<TC>>();
+        tests.run();
+    } catch (const std::exception& ex) {
+        WARN(ex.what());
+        throw;
+    }
+}
+
+}  // namespace genny::testing
+
+
+#endif  // HEADER_8A80B79E_1A24_4745_BB59_B1FCD1439C6D_INCLUDED
