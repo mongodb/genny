e59351f594194ec506d7f1750f6d11a5edc93db4
Ryan Timmons <ryan.timmons@mongodb.com>
2019-04-26 16:36:55 -0400

    proper header

--- src/config/include/config/config.hpp
+++ src/config/include/config/config.hpp
@@ -1,7 +1,19 @@
-#include <utility>
+// Copyright 2019-present MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#pragma once
-// TODO: proper header
+#ifndef HEADER_17681835_40A0_443E_939D_3679A1A6B5DD_INCLUDED
+#define HEADER_17681835_40A0_443E_939D_3679A1A6B5DD_INCLUDED
 
 #include <memory>
 #include <optional>
@@ -253,3 +265,5 @@ inline NodeT::iterator NodeT::end() const {
 using Node = NodeT;
 
 }  // namespace config
+
+#endif  // HEADER_17681835_40A0_443E_939D_3679A1A6B5DD_INCLUDED
