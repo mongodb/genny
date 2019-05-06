420397c51bd5dbd6830ca31245d23ee2ce3aed3c
Ryan Timmons <ryan.timmons@mongodb.com>
2019-05-06 13:48:44 -0400

    simplify get()

--- src/config/include/config/Node.hpp
+++ src/config/include/config/Node.hpp
@@ -691,16 +691,13 @@ private:
         const std::string keyStr = v1::toString(key);
         if constexpr (std::is_convertible_v<K, std::string>) {
             if (key == "..") {
-                // this is...not the most succinct business-logic ever....
-                std::stringstream prefix;
-                if (_parent) {
-                    prefix << _parent->_key << "/";
-                }
-                prefix << _key << "/..";
                 if (!_parent) {
-                    return Node{YAML::Node{}, nullptr, false, prefix.str()};
+                    return Node{YAML::Node{}, nullptr, false, _key + "/.."};
                 }
-                return Node{_parent->_yaml, _parent->_parent, _parent->_valid, prefix.str()};
+
+                std::ostringstream parentKey;
+                parentKey << _parent->_key << "/" << _key << "/..";
+                return Node{_parent->_yaml, _parent->_parent, _parent->_valid, parentKey.str()};
             }
         }
         try {
