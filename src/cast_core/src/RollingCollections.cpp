9d8fa40525e820ab73dedb7b3247852d9dbe1a0c
Luke Pearson <luke.pearson@mongodb.com>
2019-10-23 04:24:20 +1100

    TIG-1895 Oplog tailer (#282)

--- src/cast_core/src/RollingCollections.cpp
+++ src/cast_core/src/RollingCollections.cpp
@@ -21,12 +21,14 @@
 #include <mongocxx/collection.hpp>
 #include <mongocxx/database.hpp>
 
+#include <boost/algorithm/string.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/throw_exception.hpp>
-
 #include <gennylib/Cast.hpp>
 #include <gennylib/MongoException.hpp>
 #include <gennylib/context.hpp>
+#include <metrics/metrics.hpp>
+#include <metrics/operation.hpp>
 
 #include <value_generators/DocumentGenerator.hpp>
 
@@ -50,25 +52,18 @@ struct RunOperation {
     RollingCollectionNames& rollingCollectionNames;
 };
 
+static long getMillisecondsSinceEpoch() {
+    return std::chrono::time_point_cast<std::chrono::milliseconds>(metrics::clock::now())
+        .time_since_epoch()
+        .count();
+}
+
 static std::string getRollingCollectionName() {
     // The id is tracked globally and increments for every collection created.
     static std::atomic_long id = 0;
-    std::string timestamp(20, '.');
-    auto now = std::chrono::system_clock::now();
-    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-    auto time = std::chrono::system_clock::to_time_t(now);
-    std::strftime(&timestamp[0], timestamp.size(), "%Y-%m-%d-%H:%M:%S", std::localtime(&time));
-    /*
-     * This replaces the null terminator in the string which is added by strftime and doesn't get
-     * removed by the string stream for some reason.
-     */
-    timestamp[timestamp.size() - 1] = '.';
     std::stringstream ss;
-    /*
-     * This will create a collection name looking something like: r134_2019-08-30-11:08:19.123,
-     * the setw and setfill pad 0's for the ms if its anything < 100 ms.
-     */
-    ss << "r" << id << "_" << timestamp << std::setfill('0') << std::setw(3) << ms.count();
+    ss << "r"
+       << "_" << id << "_" << getMillisecondsSinceEpoch();
     id++;
     return ss.str();
 }
@@ -154,7 +149,7 @@ struct Write : public RunOperation {
     void run() override {
         auto statTracker = _insertOperation.start();
         auto document = _documentExpr();
-        if (!rollingCollectionNames.empty()) {
+        if (rollingCollectionNames.empty()) {
             statTracker.failure();
             return;
         }
@@ -260,6 +255,65 @@ private:
     std::vector<DocumentGenerator> _indexConfig;
 };
 
+struct OplogTailer : public RunOperation {
+    OplogTailer(PhaseContext& phaseContext,
+                mongocxx::database db,
+                ActorId id,
+                RollingCollectionNames& rollingCollectionNames)
+        : RunOperation(db, rollingCollectionNames),
+          _cursor{},
+          _oplogLagOperation{phaseContext.operation("OplogLag", id)} {
+        if (phaseContext.actor()["Threads"].to<int>() != 1) {
+            BOOST_THROW_EXCEPTION(
+                InvalidConfigurationException("OplogTailer can only be run with one thread"));
+        }
+    }
+
+    void run() override {
+        if (_firstLoop) {
+            mongocxx::options::find opts{};
+            opts.cursor_type(mongocxx::cursor::type::k_tailable);
+            _cursor = std::optional<mongocxx::cursor>(database["oplog.rs"].find({}, opts));
+            /*
+             * Exhaust the cursor to skip the initially created collections,
+             * i.e. the admin / local tables and the intial set of rolling
+             * collections, this avoids a spike in latency.
+             */
+            for (auto&& doc : _cursor.value()) {
+                // Do nothing.
+            }
+            _firstLoop = false;
+        }
+        for (auto&& doc : _cursor.value()) {
+            if (doc["op"].get_utf8().value.to_string() == "c") {
+                auto object = doc["o"].get_document().value;
+                auto it = object.find("create");
+                if (it != object.end()) {
+                    auto collectionName = object["create"].get_utf8().value.to_string();
+                    if (collectionName.length() > 2 && collectionName[0] == 'r' &&
+                        collectionName[1] == '_') {
+                        // Get the time as soon as we know we its a collection we care about.
+                        auto now = metrics::clock::now();
+                        std::vector<std::string> timeSplit;
+                        boost::algorithm::split(timeSplit, collectionName, boost::is_any_of("_"));
+                        auto started = metrics::clock::time_point{
+                            std::chrono::duration_cast<metrics::clock::time_point::duration>(
+                                std::chrono::milliseconds{std::stol(timeSplit[2])})};
+                        _oplogLagOperation.report(
+                            now,
+                            std::chrono::duration_cast<std::chrono::microseconds>(now - started),
+                            metrics::OutcomeType::kSuccess);
+                    }
+                }
+            }
+        }
+    }
+
+private:
+    bool _firstLoop = true;
+    std::optional<mongocxx::cursor> _cursor;
+    metrics::Operation _oplogLagOperation;
+};
 
 std::unique_ptr<RunOperation> getOperation(const std::string& operation,
                                            PhaseContext& context,
@@ -275,6 +329,8 @@ std::unique_ptr<RunOperation> getOperation(const std::string& operation,
         return std::make_unique<Read>(context, db, id, rollingCollectionNames, random);
     } else if (operation == "Write") {
         return std::make_unique<Write>(context, db, id, rollingCollectionNames);
+    } else if (operation == "OplogTailer") {
+        return std::make_unique<OplogTailer>(context, db, id, rollingCollectionNames);
     } else {
         BOOST_THROW_EXCEPTION(InvalidConfigurationException("Unknown operation " + operation));
     }
