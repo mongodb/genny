2775e73180fa6e1c5f3fb169da119957590d0a56
Don Anderson <dda@mongodb.com>
2019-11-04 13:18:40 -0500

    TIG-2064: Add capabilities for collection scanner and rolling collections. (#295)
    New options: CollectionSkip (skip a number of collections when scanning),
    CollectionSortOrder (sort the collection names forward or backward before
    scanning), ScanDuration (hold a snapshot scan transaction open until a
    duration has elapsed).
    Modified option: Allow Database to be a single database name or a comma
    separated list of databases to scan.
    Rolling Collections now are named in a manner that gives a time based
    ordering when sorted by name. This is needed by the large scale workload
    so collections can be traversed "ahead" of when they are expiring.
    Includes test for Collection Scanner.

--- src/cast_core/src/RollingCollections.cpp
+++ src/cast_core/src/RollingCollections.cpp
@@ -62,8 +62,8 @@ static std::string getRollingCollectionName() {
     // The id is tracked globally and increments for every collection created.
     static std::atomic_long id = 0;
     std::stringstream ss;
-    ss << "r"
-       << "_" << id << "_" << getMillisecondsSinceEpoch();
+    // Create a unique collection name that sorts lexographically by time.
+    ss << "r_" << getMillisecondsSinceEpoch() << "_" << id;
     id++;
     return ss.str();
 }
@@ -292,13 +292,13 @@ struct OplogTailer : public RunOperation {
                     auto collectionName = object["create"].get_utf8().value.to_string();
                     if (collectionName.length() > 2 && collectionName[0] == 'r' &&
                         collectionName[1] == '_') {
-                        // Get the time as soon as we know we its a collection we care about.
+                        // Get the time as soon as we know it's a collection we care about.
                         auto now = metrics::clock::now();
                         std::vector<std::string> timeSplit;
                         boost::algorithm::split(timeSplit, collectionName, boost::is_any_of("_"));
                         auto started = metrics::clock::time_point{
                             std::chrono::duration_cast<metrics::clock::time_point::duration>(
-                                std::chrono::milliseconds{std::stol(timeSplit[2])})};
+                                std::chrono::milliseconds{std::stol(timeSplit[1])})};
                         _oplogLagOperation.report(
                             now,
                             std::chrono::duration_cast<std::chrono::microseconds>(now - started),
