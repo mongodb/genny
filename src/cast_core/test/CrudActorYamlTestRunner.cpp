e4e5058db6369cbd7a3a6356e35d56198c68850b
Robert Guo <robert.guo@10gen.com>
2020-11-05 09:52:52 -0500

    int64 for wtimeout

--- src/cast_core/test/CrudActorYamlTestRunner.cpp
+++ src/cast_core/test/CrudActorYamlTestRunner.cpp
@@ -98,7 +98,7 @@ void requireEvent(ApmEvent& event, YAML::Node requirements) {
         REQUIRE(event.command["writeConcern"]["j"].get_bool().value == j.as<bool>());
     }
     if (auto wtimeout = requirements["writeConcern"]["wtimeout"]; wtimeout) {
-        REQUIRE(event.command["writeConcern"]["wtimeout"].get_int32().value == wtimeout.as<int32_t>());
+        REQUIRE(event.command["writeConcern"]["wtimeout"].get_int64().value == wtimeout.as<int64_t>());
     }
     if (auto ordered = requirements["ordered"]; ordered) {
         REQUIRE(event.command["ordered"].get_bool() == ordered.as<bool>());
