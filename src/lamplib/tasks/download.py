dbf1732dc713243effa4c251907a9b5e31643e9d
Carl Raiden Worley <carl.worley@10gen.com>
2020-11-19 20:55:41 -0500

    revendor curator

--- src/lamplib/tasks/download.py
+++ src/lamplib/tasks/download.py
@@ -220,7 +220,7 @@ class CuratorDownloader(Downloader):
 
     # Note that DSI also downloads Curator, the location is specified in defaults.yml.
     # Please try to keep the two versions consistent.
-    CURATOR_VERSION = "8b3ef73c55fdc45ef78383b0dcd4a84335b8eecb"
+    CURATOR_VERSION = "d3da25b63141aa192c5ef51b7d4f34e2f3fc3880"
     CURATOR_ROOT = os.getcwd()
 
     def __init__(self, os_family, distro):
