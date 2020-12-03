10375ab629473498ccb883dbbde245842ff60a14
Alex Cameron <alex.cameron@10gen.com>
2020-12-03 11:31:34 +0000

    WT-6966 Begin accepting a ScanRate parameter.

--- src/cast_core/src/CollectionScanner.cpp
+++ src/cast_core/src/CollectionScanner.cpp
@@ -66,6 +66,7 @@ struct CollectionScanner::PhaseConfig {
     SortOrderType sortOrder;
     int64_t collectionSkip;
     std::optional<TimeSpec> scanDuration;
+    std::optional<RateSpec> scanRate;
     bool selectClusterTimeOnly = false;
     bool queryCollectionList;
     std::optional<mongocxx::options::transaction> transactionOptions;
@@ -85,6 +86,7 @@ struct CollectionScanner::PhaseConfig {
           scanSizeBytes{context["ScanSizeBytes"].maybe<IntegerSpec>().value_or(0)},
           collectionSkip{context["CollectionSkip"].maybe<IntegerSpec>().value_or(0)},
           scanDuration{context["ScanDuration"].maybe<TimeSpec>()},
+          scanRate{context["ScanRate"].maybe<RateSpec>()},
           selectClusterTimeOnly{context["SelectClusterTimeOnly"].maybe<bool>().value_or(false)} {
         // The list of databases is comma separated.
         std::vector<std::string> dbnames;
@@ -116,6 +118,10 @@ struct CollectionScanner::PhaseConfig {
             scanType = ScanType::kPointInTime;
         } else if (!selectClusterTimeOnly) {
             BOOST_THROW_EXCEPTION(InvalidConfigurationException("ScanType is invalid."));
+        }
+        if (scanDuration && scanRate) {
+            BOOST_THROW_EXCEPTION(
+                InvalidConfigurationException("ScanDuration and ScanRate are mutually exclusive."));
         }
         if (scanDuration) {
             if (scanDuration->count() < 0) {
@@ -189,6 +195,10 @@ void collectionScan(CollectionScanner::PhaseConfig* config,
     size_t scanSize = 0;
     bool scanFinished = false;
     auto statTracker = config->scanOperation.start();
+    /*
+     * tetsuo-cpp: Figure out frequently we're inserting and then add some sleeps to ensure that
+     * we're keeping in line with the scan rate.
+     */
     for (auto& collection : collections) {
         auto filter = config->filterExpr ? config->filterExpr->evaluate()
                                          : bsoncxx::document::view_or_value{};
