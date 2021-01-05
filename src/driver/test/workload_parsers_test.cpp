599d9c354fd93031515c1285efcc4113567a9ea9
Carl Raiden Worley <carl.worley@10gen.com>
2021-01-05 09:16:02 -0500

    Add yaml-based scope test

--- src/driver/test/workload_parsers_test.cpp
+++ src/driver/test/workload_parsers_test.cpp
@@ -102,6 +102,94 @@ Actors:
     REQUIRE(YAML::Dump(parsedConfig) == expected);
 }
 
+TEST_CASE("WorkloadParser parameters are scoped") {
+    const auto input = (R"(
+ActorTemplates:
+- TemplateName: TestTemplate1
+  Config:
+    Name: {^Parameter: {Name: "Name", Default: "DefaultValue"}}
+    SomeKey: SomeValue
+    Phases:
+      OnlyIn:
+        Active: [{^Parameter: {Name: "Phase", Default: 1}}]
+        Max: 3
+        Config:
+          Duration: {^Parameter: {Name: "Duration", Default: 3 minutes}}
+
+- TemplateName: TestTemplate2
+  Config:
+    Name: {^Parameter: {Name: "Name", Default: "DefaultValue"}}
+    SomeKey: SomeValue
+    Phases:
+      - Nop: true
+      - Nop: true
+      - ExternalPhaseConfig:
+          Path: src/testlib/phases/Good.yml
+          Parameters:
+            Repeat: 2
+      - Nop: true
+    AnotherValueFromRepeat: {^Parameter: {Name: "Repeat", Default: "BadDefault"}}
+
+Actors:
+- ActorFromTemplate:
+    TemplateName: TestTemplate1
+    TemplateParameters:
+      Name: ActorName1
+      Phase: 0
+      Duration: 5 minutes
+
+# Lacking the specified duration, we expect the default duration to be used,
+# instead of the one from the previous ActorFromTemplate which was scoped to that block.
+- ActorFromTemplate:
+    TemplateName: TestTemplate1
+    TemplateParameters:
+      Phase: 1
+      Name: ActorName2
+
+# The value of Repeat should be correctly "shadowed" in the lower level external phase.
+- ActorFromTemplate:
+    TemplateName: TestTemplate2
+    TemplateParameters:
+      Name: ActorName3
+      Repeat: GoodValue
+)");
+
+    const auto expected = (R"(Actors:
+  - Name: ActorName1
+    SomeKey: &1 SomeValue
+    Phases:
+      - Duration: 5 minutes
+      - &2
+        Nop: true
+      - *2
+      - *2
+  - Name: ActorName2
+    SomeKey: *1
+    Phases:
+      - &3
+        Nop: true
+      - Duration: 3 minutes
+      - *3
+      - *3
+  - Name: ActorName3
+    SomeKey: SomeValue
+    Phases:
+      - Nop: true
+      - Nop: true
+      - Repeat: 2
+        Mode: NoException
+      - Nop: true
+    AnotherValueFromRepeat: GoodValue)");
+
+    auto cwd = boost::filesystem::current_path();
+
+    WorkloadParser p{cwd};
+    auto parsedConfig = p.parse(input, DefaultDriver::ProgramOptions::YamlSource::kString);
+
+    REQUIRE(YAML::Dump(parsedConfig) == expected);
+}
+
+
 TEST_CASE("WorkloadParser can preprocess keywords") {
     const auto input = (R"(
 ActorTemplates:
