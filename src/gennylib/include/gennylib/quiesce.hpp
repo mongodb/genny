514c6870f9c4cf40cbbc0b10190940d01c570ffa
Carl Raiden Worley <carl.worley@10gen.com>
2021-01-11 15:11:25 -0500

    added fsync

--- src/gennylib/include/gennylib/quiesce.hpp
+++ src/gennylib/include/gennylib/quiesce.hpp
@@ -15,17 +15,46 @@
 #ifndef HEADER_5936639A_7D22_4629_8FE1_4A08443DDB0F_INCLUDED
 #define HEADER_5936639A_7D22_4629_8FE1_4A08443DDB0F_INCLUDED
 
+#include <string_view>
+
+#include <mongocxx/uri.hpp>
+#include <mongocxx/client.hpp>
 #include <mongocxx/database.hpp>
+#include <bsoncxx/types.hpp>
+#include <bsoncxx/array/view.hpp>
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/builder/basic/kvp.hpp>
 
 namespace genny {
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
+
+void doFSyncReplicaSet(mongocxx::pool::entry& client) {
+    auto admin = client->database("admin");
+    auto status = admin.run_command(make_document(kvp("replSetGetStatus", 1))).view();
+    if (!status["ok"].get_bool()) {
+        // TODO: Throw exception
+    }
+    if (status.find("members") != status.cend()) {
+        bsoncxx::array::view members = status["members"].get_array();
+        for (auto member : members) {
+            auto name = member["name"].get_utf8().value;
+            mongocxx::uri uri(name);
+            mongocxx::client memberClient(uri);
+            auto memberAdmin = memberClient.database("admin");
+            memberAdmin.run_command(make_document(kvp("fsync", 1)));
+        }
+    }
+}
 
 /*
  * Helper function to quiesce the system and reduce noise.
  * The appropriate actions will be taken whether the target
  * is a standalone, replica set, or sharded cluster.
  */
-template <class database = mongocxx::database>
-bool quiesceImpl(database& targetDb) {
+template <class entry = mongocxx::pool::entry>
+bool quiesceImpl(entry& client) {
     return true;
 }
 
