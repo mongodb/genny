932e157ce0c608c64d063a027f4f83f4a7feb079
Carl Raiden Worley <carl.worley@10gen.com>
2021-01-13 07:28:08 -0500

    topology cpp

--- src/gennylib/include/gennylib/topology.hpp
+++ src/gennylib/include/gennylib/topology.hpp
@@ -1,3 +1,18 @@
+// Copyright 2021-present MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 #ifndef HEADER_6D7AD2AF_7CD1_49E4_9712_741ABD354DFC_INCLUDED
 #define HEADER_6D7AD2AF_7CD1_49E4_9712_741ABD354DFC_INCLUDED
 
@@ -16,6 +31,14 @@ class MongosDescription;
 class ReplSetDescription;
 class ShardedDescription;
 
+/**
+ * Inherit from TopologyVisitor and override particular visit methods
+ * to implement an algorithm that operates on a cluster. Pass the visitor
+ * to a Topology object to execute.
+ *
+ * The idea is to create a visitor that is as topology-agnostic as possible,
+ * e.g. in a replica set `visitShardedDescription` will never be called.
+ */
 class TopologyVisitor {
 public:
     virtual void visitMongodDescription(const MongodDescription&) {}
@@ -68,14 +91,25 @@ struct ShardedDescription : public AbstractTopologyDescription {
     }
 };
 
+/**
+ * Represents the topology of a MongoDB cluster.
+ */
 class Topology {
+
+    /**
+     * Traverse the cluster, using the visitor to act on it.
+     */
     void accept(TopologyVisitor& v) { _topology->accept(v); }
-    void update(mongocxx::pool::entry& client) {}
+
+    /**
+     * Update the Topology's view of the cluster.
+     */
+    void update(mongocxx::pool::entry& client);
 
 private:
     std::shared_ptr<AbstractTopologyDescription> _topology;
 };
 
-}
+} // namespace genny
 
 #endif  // HEADER_6D7AD2AF_7CD1_49E4_9712_741ABD354DFC_INCLUDED
