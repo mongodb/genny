ec12b0af11e5adfe2c16b3e33cf26682afdfacd6
Ryan Timmons <ryan.timmons@mongodb.com>
2021-01-20 14:30:52 -0500

    Kill jasper and option of building without ssl

--- cmake/GetSsl.cmake
+++ cmake/GetSsl.cmake
@@ -18,15 +18,14 @@
 # OPENSSL_ROOT_DIR and OPENSSL_USE_STATIC_LIBS are often important
 find_package(OpenSSL 1.1 REQUIRED)
 
-if(GENNY_INSTALL_SSL)
-    get_filename_component(OPENSSL_CRYPTO_LIBRARY_REALPATH
-                           "${OPENSSL_CRYPTO_LIBRARY}" REALPATH CACHE
-    )
-    get_filename_component(OPENSSL_SSL_LIBRARY_REALPATH
-                           "${OPENSSL_SSL_LIBRARY}" REALPATH CACHE
-    )
-    install(FILES
-                "${OPENSSL_CRYPTO_LIBRARY_REALPATH}"
-                "${OPENSSL_SSL_LIBRARY_REALPATH}"
-            DESTINATION ${CMAKE_INSTALL_LIBDIR})
-endif()
+get_filename_component(OPENSSL_CRYPTO_LIBRARY_REALPATH
+                       "${OPENSSL_CRYPTO_LIBRARY}" REALPATH CACHE
+)
+get_filename_component(OPENSSL_SSL_LIBRARY_REALPATH
+                       "${OPENSSL_SSL_LIBRARY}" REALPATH CACHE
+)
+install(FILES
+            "${OPENSSL_CRYPTO_LIBRARY_REALPATH}"
+            "${OPENSSL_SSL_LIBRARY_REALPATH}"
+        DESTINATION ${CMAKE_INSTALL_LIBDIR})
+
