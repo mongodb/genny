18f4a2da3cf3748e06981895f725ee843e264a4c
Ryan Timmons <ryan.timmons@mongodb.com>
2021-02-03 19:55:22 -0500

    update to latest version of curator

--- src/lamplib/src/genny/curator.py
+++ src/lamplib/src/genny/curator.py
@@ -118,7 +118,7 @@ class CuratorDownloader(Downloader):
 
     # Note that DSI also downloads Curator, the location is specified in defaults.yml.
     # Please try to keep the two versions consistent.
-    CURATOR_VERSION = "d3da25b63141aa192c5ef51b7d4f34e2f3fc3880"
+    CURATOR_VERSION = "65f8da42ad320f03190ae71e1af68b69e293a942"
 
     def __init__(
         self,
