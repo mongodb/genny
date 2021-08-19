286df4a1a2ef689eef04e26605c3f62dcf79d079
Marcos José Grillo Ramirez <marcos.grillo@mongodb.com>
2021-08-19 13:56:56 +0200

    Fixed identation issue on unittest yml file

--- src/cast_core/test/MoveRandomChunkToRandomShard_test.cpp
+++ src/cast_core/test/MoveRandomChunkToRandomShard_test.cpp
@@ -38,22 +38,22 @@ TEST_CASE_METHOD(MongoTestFixture,
         SchemaVersion: 2018-07-01
         Actors:
         - Name: CreateShardedCollection
-        Type: AdminCommand
-        Threads: 1
-        Phases:
-        - Repeat: 1
-            Database: admin
-            Operations:
-            - OperationMetricsName: EnableSharding
-            OperationName: AdminCommand
-            OperationCommand:
-                enableSharding: test
-            - OperationMetricsName: ShardCollection
-            OperationName: AdminCommand
-            OperationCommand:
-                shardCollection: test.collection0
-                key: {Key: 1}
-        - {Nop: true}
+          Type: AdminCommand
+          Threads: 1
+          Phases:
+          - Repeat: 1
+              Database: admin
+              Operations:
+              - OperationMetricsName: EnableSharding
+              OperationName: AdminCommand
+              OperationCommand:
+                  enableSharding: test
+              - OperationMetricsName: ShardCollection
+              OperationName: AdminCommand
+              OperationCommand:
+                  shardCollection: test.collection0
+                  key: {Key: 1}
+          - {Nop: true}
 
         - Name: MoveRandomChunkToRandomShard
           Type: MoveRandomChunkToRandomShard
