8b7692df16218c3bf891ecd9ad55847a979a97a9
Carl Raiden Worley <carl.worley@10gen.com>
2021-11-07 17:14:01 -0500

    add parallel tests

--- src/gennylib/test/parallel_test.cpp
+++ src/gennylib/test/parallel_test.cpp
@@ -27,6 +27,7 @@
 
 using namespace genny;
 using Catch::Matchers::Matches;
+using Catch::Matchers::Contains;
 
 TEST_CASE("Parallel runner runs op") {
     std::vector<int> integers = {1, 2, 3, 4, 5};
@@ -56,4 +57,73 @@ TEST_CASE("Parallel runner reraises exceptions") {
                    });
     };
     REQUIRE_THROWS_WITH(test(), Matches("This should be reraised."));
+}
+
+TEST_CASE("AtomicContainer throws if iterating without holding lock") {
+    AtomicVector<int> integers;
+    integers.push_back(6);
+    integers.push_back(7);
+    auto mustHoldLock = [&]() {
+        for (auto&& integer : integers) {
+            
+        }
+    };
+    REQUIRE_THROWS_WITH(mustHoldLock(), Contains("Must forcibly hold container lock in order to iterate."));
+
+    auto cannotLockTwice = [&]() {
+        std::lock_guard<AtomicVector<int>> intLock1(integers);
+        std::lock_guard<AtomicVector<int>> intLock2(integers);
+    };
+
+    REQUIRE_THROWS_WITH(cannotLockTwice(), Contains("Cannot forcibly hold AtomicContainer lock multiple times."));
+
+    AtomicVector<int> newIntegers;
+    auto safelyIterates = [&]() {
+        std::lock_guard<AtomicVector<int>> intLock(integers);
+        for (auto&& integer : integers) {
+            newIntegers.push_back(integer + 1);
+        }
+    };
+    safelyIterates();
+    REQUIRE(newIntegers.size() == 2);
+    REQUIRE(newIntegers[0] == 7);
+    REQUIRE(newIntegers[1] == 8);
+}
+
+TEST_CASE("AtomicContainer locks when used") {
+    using namespace std::chrono_literals;
+    // One thread that starts soon, another thread that waits then is held up.
+    std::vector<std::chrono::milliseconds> millis = {100ms, 400ms};
+    AtomicVector<std::chrono::milliseconds> outputs;
+    AtomicVector<std::chrono::milliseconds> outputs2;
+    auto test = [&]() {
+        parallelRun(millis,
+                   [&](const auto& millis) {
+                       std::this_thread::sleep_for(millis);
+                       // Second thread should block here.
+                       outputs.push_back(millis);
+                       std::lock_guard<AtomicVector<std::chrono::milliseconds>> lk(outputs);
+                       outputs.push_back(millis);
+                       std::this_thread::sleep_for(600ms);
+                       for (const auto& i : outputs) {
+                           outputs2.push_back(millis);
+                       }
+                   });
+    };
+
+    test();
+
+    std::vector<std::chrono::milliseconds> expected = {100ms, 100ms, 400ms, 400ms};
+    std::vector<std::chrono::milliseconds> expected2 = {100ms, 100ms, 400ms, 400ms, 400ms, 400ms};
+    REQUIRE(expected.size() == outputs.size());
+    REQUIRE(expected2.size() == outputs2.size());
+
+    std::lock_guard<AtomicVector<std::chrono::milliseconds>> lk(outputs);
+    for (int i = 0; i < expected.size(); i++) {
+        REQUIRE(outputs[i] == expected[i]);
+    }
+    std::lock_guard<AtomicVector<std::chrono::milliseconds>> lk2(outputs2);
+    for (int i = 0; i < expected2.size(); i++) {
+        REQUIRE(outputs2[i] == expected2[i]);
+    }
 }
