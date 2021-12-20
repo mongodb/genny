a26a2c2d98efb2c1b1669b0b9c4de1b8db3593c6
Max Hirschhorn <max.hirschhorn@mongodb.com>
2021-12-20 16:18:37 +0000

    Beginnings of C++ testing.

--- src/cast_core/test/GetMoreActor_test.cpp
+++ src/cast_core/test/GetMoreActor_test.cpp
@@ -12,15 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <bsoncxx/json.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/basic/document.hpp>
 
 #include <boost/exception/diagnostic_information.hpp>
 
 #include <yaml-cpp/yaml.h>
 
-#include <testlib/MongoTestFixture.hpp>
 #include <testlib/ActorHelper.hpp>
+#include <testlib/MongoTestFixture.hpp>
 #include <testlib/helpers.hpp>
 
 #include <gennylib/context.hpp>
@@ -28,51 +27,39 @@
 namespace genny {
 namespace {
 using namespace genny::testing;
-namespace bson_stream = bsoncxx::builder::stream;
 
-//
-// ⚠️ There is a "known" failure that you should find and fix as a bit of
-// an exercise in reading and testing your Actor. ⚠️
-//
+TEST_CASE_METHOD(MongoTestFixture,
+                 "GetMoreActor supports running find command",
+                 "[standalone][single_node_replset][three_node_replset][sharded][GetMoreActor]") {
 
-TEST_CASE_METHOD(MongoTestFixture, "GetMoreActor successfully connects to a MongoDB instance.",
-          "[standalone][single_node_replset][three_node_replset][sharded][GetMoreActor]") {
-
-    dropAllDatabases();
-    auto db = client.database("mydb");
-
-    NodeSource nodes = NodeSource(R"(
+    NodeSource config = NodeSource(R"(
         SchemaVersion: 2018-07-01
         Actors:
         - Name: GetMoreActor
           Type: GetMoreActor
-          Database: mydb
           Phases:
-          - Repeat: 100
-            Collection: mycoll
-            Document: {foo: {^RandomInt: {min: 0, max: 100}}}
-    )", __FILE__);
+          - Repeat: 1
+            Database: mydb
+            InitialCursorCommand:
+              find: mycoll
+    )",
+                                   __FILE__);
 
 
-    SECTION("Inserts documents into the database.") {
-        try {
-            genny::ActorHelper ah(nodes.root(), 1, MongoTestFixture::connectionUri().to_string());
-            ah.run([](const genny::WorkloadContext& wc) { wc.actors()[0]->run(); });
+    SECTION("Succeeds when collection is empty") {
+        dropAllDatabases();
 
-            auto builder = bson_stream::document{};
-            builder << "foo" << bson_stream::open_document
-                    << "$gte" << "0" << bson_stream::close_document
-                    << bson_stream::finalize;
+        auto events = ApmEvents{};
+        auto apmCallback = makeApmCallback(events);
+        genny::ActorHelper ah(
+            config.root(), 1, MongoTestFixture::connectionUri().to_string(), apmCallback);
 
-            auto count = db.collection("mycoll").count_documents(builder.view());
-            // TODO: fixme
-            REQUIRE(count == 101);
-        } catch (const std::exception& e) {
-            auto diagInfo = boost::diagnostic_information(e);
-            INFO("CAUGHT " << diagInfo);
-            FAIL(diagInfo);
-        }
+        ah.run([](const genny::WorkloadContext& wc) { wc.actors()[0]->run(); });
+
+        REQUIRE(events.size() == 1U);
+        REQUIRE(events[0].command_name == "find");
     }
 }
+
 }  // namespace
 }  // namespace genny
