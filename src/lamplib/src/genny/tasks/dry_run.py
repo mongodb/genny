3c132e11083d929c89fb5d58843ecdf88e86199f
Ryan Timmons <ryan.timmons@10gen.com>
2022-06-06 13:11:39 -0400

    also skip Mixed10KThreads.yml

--- src/lamplib/src/genny/tasks/dry_run.py
+++ src/lamplib/src/genny/tasks/dry_run.py
@@ -32,6 +32,7 @@ def dry_run_workload(
         "AuthNInsert.yml",
         "ParallelInsert.yml",
         "LoggingActorExample.yml",
+        "Mixed10KThreads.yml",
     ]:
         SLOG.info("TIG-1435 skipping dry run on macOS", file=yaml_file_path)
         return
