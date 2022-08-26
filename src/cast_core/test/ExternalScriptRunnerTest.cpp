335085dc1c6c8cdac77bb8966716dd42a07d126c
Xiaochen Wu <allenwu.x@gmail.com>
2022-08-26 15:30:41 +0000

    remove not needed comments

--- src/cast_core/test/ExternalScriptRunnerTest.cpp
+++ src/cast_core/test/ExternalScriptRunnerTest.cpp
@@ -30,11 +30,6 @@ namespace {
 using namespace genny::testing;
 namespace bson_stream = bsoncxx::builder::stream;
 
-//
-// ⚠️ There is a "known" failure that you should find and fix as a bit of
-// an exercise in reading and testing your Actor. ⚠️
-//
-
 TEST_CASE_METHOD(MongoTestFixture, "ExternalScriptRunner successfully executed.",
           "[standalone][single_node_replset][three_node_replset][sharded][ExternalScriptRunner]") {
 
