2cda3c1ef5ffcb889850f57f9e56e3ef3829e215
William Qian <william.qian@mongodb.com>
2023-07-27 12:45:35 -0400

    PERF-4466 Implement support for binary data type Sensitive and use this for query stats HMAC key generation

--- src/value_generators/src/DocumentGenerator.cpp
+++ src/value_generators/src/DocumentGenerator.cpp
@@ -1176,25 +1176,27 @@ private:
 
 /** `{^BinData: {numBytes: 32}}` */
 class BinDataGenerator : public Generator<bsoncxx::types::b_binary> {
+private:
+    using bintype = bsoncxx::binary_sub_type;
+
 public:
-    BinDataGenerator(const Node& node, GeneratorArgs generatorArgs)
-        : _node{node} {
-            _binData = genRandBinData();
-        }
+    BinDataGenerator(const Node& node, GeneratorArgs generatorArgs,
+                     const bintype binDataType = bintype::k_binary)
+        : _node{node}, _binData(genRandBinData(binDataType)) {}
 
     bsoncxx::types::b_binary evaluate() override {
         return _binData;
     }
 
-    bsoncxx::types::b_binary genRandBinData() {
+    bsoncxx::types::b_binary genRandBinData(const bintype binDataType) {
         int64_t numBytes = _node["numBytes"].maybe<int64_t>().value_or(32);
         uint8_t bytesArr[numBytes];
         for (int i = 0; i < numBytes; i++) {
             bytesArr[i] = rand();
         }
-        return bsoncxx::types::b_binary{bsoncxx::binary_sub_type::k_binary,
-                                                     static_cast<uint32_t>(sizeof(bytesArr)),
-                                                     bytesArr};
+        return bsoncxx::types::b_binary{binDataType,
+                                        static_cast<uint32_t>(sizeof(bytesArr)),
+                                        bytesArr};
     }
 
 private:
@@ -1778,6 +1780,11 @@ const static std::map<std::string, Parser<UniqueAppendable>> allParsers{
      {"^BinData",
      [](const Node& node, GeneratorArgs generatorArgs) {
          return std::make_unique<BinDataGenerator>(node, generatorArgs);
+     }},
+     {"^BinDataSensitive",
+     [](const Node& node, GeneratorArgs generatorArgs) {
+         return std::make_unique<BinDataGenerator>(
+            node, generatorArgs, bsoncxx::binary_sub_type(0x8));
      }},
 };
 
