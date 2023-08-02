81e1b21e0a5983bb3eeb5070f31af721d612c647
Minh Luu <PyGeek03@users.noreply.github.com>
2023-08-02 18:01:37 +1000

    Batch miscellaneous fixes for Genny (#925)

--- src/metrics/test/metrics_test.cpp
+++ src/metrics/test/metrics_test.cpp
@@ -492,7 +492,8 @@ TEST_CASE("Phases can set metrics") {
         ah.run();
 
         const auto metrics = ah.getMetricsOutput();
-        REQUIRE_THAT(std::string(metrics), Catch::Matchers::ContainsSubstring("Phase1Metrics_bytes,13"));
+        REQUIRE_THAT(std::string(metrics),
+                     Catch::Matchers::ContainsSubstring("Phase1Metrics_bytes,13"));
     }
 
     SECTION("With default metrics name") {
@@ -516,7 +517,8 @@ TEST_CASE("Phases can set metrics") {
         ah.run();
 
         const auto metrics = ah.getMetricsOutput();
-        REQUIRE_THAT(std::string(metrics), Catch::Matchers::ContainsSubstring("DefaultMetricsName.0_bytes,13"));
+        REQUIRE_THAT(std::string(metrics),
+                     Catch::Matchers::ContainsSubstring("DefaultMetricsName.0_bytes,13"));
     }
 }
 
