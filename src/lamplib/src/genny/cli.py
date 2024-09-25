d97681683b59a826e991b4f57ce114dcc894927a
Simon Eismann <simon.eismann@mongodb.com>
2024-09-25 13:57:50 +0200

    Make sure we clone the newest version when in evergreen.

--- src/lamplib/src/genny/cli.py
+++ src/lamplib/src/genny/cli.py
@@ -671,17 +671,13 @@ def auto_tasks_all(ctx: click.Context, project_files: List[str], no_activate: bo
     name="auto-tasks-local",
     help=("Regenerate the auto-generated evergreen task defintions."),
 )
-@click.option(
-    "--evergreen", default=False, is_flag=True, help="Don't check out repositories, since we are running in evergreen"
-)
 @click.pass_context
-def auto_tasks_local(ctx: click.Context, evergreen: bool):
+def auto_tasks_local(ctx: click.Context):
     from genny.tasks import auto_tasks_local
     import sys
     print(sys.version)
     auto_tasks_local.main(
-        workspace_root=ctx.obj["WORKSPACE_ROOT"],
-        running_in_evergreen=evergreen
+        workspace_root=ctx.obj["WORKSPACE_ROOT"]
     )
 
 
